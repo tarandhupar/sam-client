5aae49fe3cf34ca3f394182ac2308968a4266ffb
nithinemu <nitin.emmanuel@gmail.com>
2016-09-14 16:24:51 -0400

    Common Components Added
    Common Components Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,10 @@ import { InputTypeConstants } from './common/constants/input.type.constants.ts';
 import { SamFooter } from './common/samuikit/sam-footer.ts';
 import { SamHeader } from './common/samuikit/sam-header.ts';
 import { SamSpace } from './common/samuikit/sam-space.ts';
+import { SamButton } from './common/samuikit/sam-button.ts';
+import { SamLabel } from './common/samuikit/sam-label.ts';
+import { SamAccordions } from './common/samuikit/sam-accordions.ts';
+import { SamSelect } from './common/samuikit/sam-select.ts';
 import '../assets/js/samuikit.js';
 
 
@@ -22,12 +26,30 @@ import '../assets/js/samuikit.js';
   ],
   templateUrl: 'app.template.html',
   providers : [ComponentInjectService,InputTypeConstants],
-  directives: [SamHeader, SamFooter, SamSpace]
+  directives: [SamHeader, SamFooter, SamSpace, SamButton, SamLabel, SamAccordions, SamSelect ]
 })
 export class App {
   testValue = { value: 'Test' };
+  buttonValue = {type:"default", data:"Default"};
+  labelValue = {type:"small", data:"Day"};
+  accordionsValue = {accordions: [
+                                          {title:"Test1", content:"This is Test1",expanded:false},
+                                          {title:"Test2", content:"This is Test2", expanded:true},
+                                          {title:"Test3", content:"This is Test3",expanded:false}
+                                        ],
+                          bordered:false };
+  selectValue = {
+                                type: 'dropdown',
+                                label: 'Dropdown label',
+                                name: 'options',
+                                options: {
+                                  'value1': 'Option A',
+                                  'value2': 'Option B',
+                                  'value3': 'Option C'
+                                }
+                              };
   constructor() {
-    
+
   }
 
   ngOnInit() {
@@ -35,4 +57,3 @@ export class App {
   }
 
 }
-
