12b0c1ede6be0aa0ae8fc7737895addf0f150b7e
tarandhupar <tarandeepdhupar@Tarandeeps-MacBook-Pro.local>
2016-09-21 16:44:14 -0400

    Merge branch 'sam-angular-module'
    # Conflicts:
    #	src/app/common/samuikit/sam-accordions.ts
    #	src/app/common/samuikit/sam-button.ts
    #	src/app/common/samuikit/sam-label.ts
    #	src/app/common/samuikit/sam-select.ts
    #	src/sam-angular/sam-select/sam-select.component.ts

--- src/app/common/samuikit/sam-accordions.ts
+++ src/app/common/samuikit/sam-accordions.ts
@@ -1,23 +1,26 @@
-import {Component, ElementRef, OnInit, Injector, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ComponentInjectService } from '../service/component.inject.service.ts';
 import { InputTypeConstants } from '../constants/input.type.constants.ts';
+import { SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'samAccordions',
-  template:`<div id={{labelname}}></div>`,
+  template:`<div id={{labelname}} [innerHTML]='html'></div>`,
   providers: [ComponentInjectService, InputTypeConstants]
 })
-export class SamAccordions implements OnInit {
+export class SamAccordions {
 
-  @Input() labelname;
-  @Input() config : any;
+  @Input() labelname: string;
+  @Input() config: any;
+
+  html: SafeHtml;
 
   constructor(
-    public _injector:Injector,
-  ) {}
-
-  ngOnInit() {
-
-
+    private _componentInjectService : ComponentInjectService
+  ) {
+  }
+  
+  ngOnInit(){
+    this.html = this._componentInjectService.renderComponentHTML('accordions', this.config);
   }
 }
--- src/app/common/samuikit/sam-label.ts
+++ src/app/common/samuikit/sam-label.ts
@@ -1,22 +1,26 @@
-import {Component, OnInit, Injector, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ComponentInjectService } from '../service/component.inject.service.ts';
 import { InputTypeConstants } from '../constants/input.type.constants.ts';
+import { SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'samLabel',
-  template:`<div id={{labelname}}></div>`,
+  template:`<div id={{labelname}} [innerHTML]='html'></div>`,
   providers: [ComponentInjectService, InputTypeConstants]
 })
-export class SamLabel implements OnInit {
+export class SamLabel {
 
-  @Input() labelname;
-  @Input() config : any;
+  @Input() labelname: string;
+  @Input() config: any;
+
+  html: SafeHtml;
 
   constructor(
-    public _injector:Injector,
     private _componentInjectService : ComponentInjectService
-  ) {}
+  ) {
+  }
 
-  ngOnInit() {
+  ngOnInit(){
+    this.html = this._componentInjectService.renderComponentHTML('label', this.config);
   }
 }
