48871d4ffcfe432fc7352634a06164850cbf539c
Diego Ruiz <diego.ruiz@reisystems.com>
2016-09-22 10:14:22 -0400

    SGS-10 tweaks for unit testing

--- src/app/search/assistance_listings/al.spec.ts
+++ src/app/search/assistance_listings/al.spec.ts
@@ -7,6 +7,7 @@ import { AssistanceListingResult } from './al.component';
 var fixture;
 var comp;
 var titleEl;
+var programNumberEl;
 describe('AssistanceListingResultComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -15,16 +16,21 @@ describe('AssistanceListingResultComponent', () => {
 
     fixture = TestBed.createComponent(AssistanceListingResult);
     comp = fixture.componentInstance; 
-    titleEl  = fixture.debugElement.query(By.css('.fal-program-number')); // find title element
-
+    titleEl  = fixture.debugElement.query(By.css('.assistance-listing-title')); // find title element
+    programNumberEl = fixture.debugElement.query(By.css((".fal-program-number"));
     comp.data = {
-    	programNumber: "12345",
+    	title: "ABCDEFG",
+      programNumber: "12345",
       archive: false
     };
     fixture.detectChanges();// trigger data binding
   });
 
-  it('should display a programNumber', () => {
-	  //expect(titleEl.nativeElement.textContent).toContain("12345");
+  it('should display a title', () => {
+	  expect(titleEl.nativeElement.textContent).toContain("ABCDEFG");
 	});
+
+  it('should display a programNumber', () => {
+    expect(programNumberEl.nativeElement.textContent).toContain("12345");
+  });
 });
