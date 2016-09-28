6cd893638487b335c8533edb1c24f578c9fe2b29
jahangir-iqbal-rei <jahangir.iqbal@reisystems.com>
2016-09-28 11:57:51 -0400

    unit tests for historical-index.service.ts initial

---
+++ src/app/fal/services/historical-index.service.spec.ts
@@ -0,0 +1,52 @@
+import { HistoricalIndexService } from './historical-index.service';
+import {APIService} from '../../common/service/api/api.service'
+import {Http, Headers, RequestOptions, Request, RequestMethod, Response, URLSearchParams} from '@angular/http';
+
+
+import { inject, TestBed } from '@angular/core/testing';
+import { BaseRequestOptions, ResponseOptions } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+
+
+describe('HistoricalIndexService unit tests without TestBed', () => {
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        HistoricalIndexService,
+        APIService,
+        BaseRequestOptions,
+        MockBackend,
+        {
+          provide: Http,
+          useFactory: (backend: MockBackend, defaultOptions: BaseRequestOptions) => {
+            return new Http(backend, defaultOptions);
+          },
+          deps: [MockBackend, BaseRequestOptions],
+        },
+      ],
+    });
+  });
+
+  beforeEach(inject([MockBackend], (backend: MockBackend) => {
+    const baseResponse = new Response(new ResponseOptions({ body: '{"response":"sot response!!"}' }));
+    backend.connections.subscribe((c: MockConnection) => c.mockRespond(baseResponse));
+  }));
+
+
+
+
+
+  it('should return response when subscribed to getHistoricalIndexByProgramNumber', inject([HistoricalIndexService], (testService: HistoricalIndexService) => {
+    testService.getHistoricalIndexByProgramNumber("5eb2b1a06998d59eb179a8e7fd76c173", "11.111").subscribe((res: Response) => {
+      expect(res['response']).toBe('sot response!!');
+    });
+  }));
+
+
+
+
+
+
+});
