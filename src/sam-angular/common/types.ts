639954ccfa91ec64339ada80d8945a6ae4ed8775
Justin Babbs <justin.babbs@octoconsulting.com>
2016-10-04 22:42:37 -0400

    remove unused type

--- src/sam-angular/common/types.ts
+++ src/sam-angular/common/types.ts
@@ -4,10 +4,3 @@ export type OptionsType = Array<{
   name: string, // the machine readable description of the input
   disabled?: boolean // if true, the option is greyed out and not clickable
 }>;
-
-export type WrapperConfigType = {
-  label: string, // the text value of the legend or the label
-  name?: string, // the machine readable description of the input
-  hint?: string,
-  errorMessage?: string,
-};
