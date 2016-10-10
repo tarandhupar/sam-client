26710135922251aadf0fcc73593eefab5d9f6d18
Justin Babbs <justin.babbs@octoconsulting.com>
2016-10-10 16:09:04 -0400

    css fixes for head

--- src/sam-angular/sam-header/sam-header.component.ts
+++ src/sam-angular/sam-header/sam-header.component.ts
@@ -3,12 +3,12 @@ import { Component, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'samHeader2',
   template: `
-    <header class="p_T-5x">
-      <div class="usa-grid-full">
-        <a class="image-wrap m_T-1x pull-left">
-          <img class="marginCenter" src="assets/img/blank_icon.png">
+    <header>
+      <div class="usa-grid">
+        <a class="pull-left sam_hat_img">
+          <img class="marginCenter" src="assets/img/sam_hat_logo.jpg">
         </a>
-        <samSearchbar class="pull-left m_L-4x m_T-4x" [size]="'small'" (onSearch)="onSearchEvent($event)"></samSearchbar>
+        <samSearchbar class="pull-left m_L-4x m_T-2x" [size]="'small'" (onSearch)="onSearchEvent($event)"></samSearchbar>      
       </div>
     </header>
 `,
