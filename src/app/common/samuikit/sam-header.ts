176a3972a067dc2033cda69ce64dfd18204ff65e
Justin Babbs <justin.babbs@octoconsulting.com>
2016-10-11 14:18:04 -0400

    move the header up when collapsing the details thingy

--- src/app/common/samuikit/sam-header.ts
+++ src/app/common/samuikit/sam-header.ts
@@ -1,28 +1,21 @@
-import {Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'samHeader',
   template:`<header class="sam-header">
-              <nav class="" aria-label="Main navigation">
-              
-                <div class="usa-width-one-fourth align-top marginCenter p_T-6x">
+              <nav class="" aria-label="Main navigation"> 
+                <div class="usa-width-one-fourth align-top marginCenter">
                     <div class="usa-width-one-half marginCenter">
                           <a class="sam-home marginCenter">
                           </a>
                     </div>
                 </div>
-               
               </nav>
             </header>`,
 })
 export class SamHeader {
 
-  @Input() labelname;
-
   constructor() {
-  }
-
-  ngOnInit(){
   }
 
 }
