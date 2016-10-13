c53aa3b9879919db2fe7797dec312552483b15b6
Diego Ruiz <diego.ruiz@reisystems.com>
2016-10-13 18:13:34 -0400

    initial commit

---
+++ src/sam-angular/sam-alert/index.ts
@@ -0,0 +1,1 @@
+export * from './sam-alert.component';
---
+++ src/sam-angular/sam-alert/sam-alert.component.ts
@@ -0,0 +1,47 @@
+import { Component, Input } from '@angular/core';
+
+/**
+ * The <samAlert> component is designed with sam.gov standards to show that this is an official website
+ * https://gsa.github.io/sam-web-design-standards/
+ *
+ * @Input type: Set alert type, defaults to 'success'
+ * @Input title: Set alert title
+ * @Input description: Set alert description
+ */
+@Component({
+  selector: 'samAlert2',
+  templateUrl: './sam-alert.template.html'
+
+})
+export class SamAlertComponent {
+  @Input() type: string;
+  @Input() title: string;
+  @Input() description: string;
+  types:any = {
+    "success":"usa-alert-success",
+    "warning":"usa-alert-warning",
+    "error":"usa-alert-error",
+    "info":"usa-alert-info"
+  }
+  selectedType: string = this.types['success'];
+  constructor() {
+  }
+
+  ngOnInit(){
+    if(!this.typeNotDefined()){
+
+      this.selectedType = this.types[this.type];
+    }
+  }
+
+  typeNotDefined(){
+    if(!this.type || this.type.length==0){
+      return true;
+    }
+    if(!this.types[this.type]){
+      return true;
+    }
+    return false;
+  }
+
+}
