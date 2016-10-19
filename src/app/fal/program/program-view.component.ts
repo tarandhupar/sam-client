0f8ba80bcf8484f7cb8c5fc12f149fb3eb4a3d69
Justin Babbs <justin.babbs@octoconsulting.com>
2016-10-19 09:35:02 -0400

    moved the pipes from common/pipes to app-pipes

--- src/app/fal/program/program-view.component.ts
+++ src/app/fal/program/program-view.component.ts
@@ -6,7 +6,7 @@ import {ProgramService} from '../services/program.service';
 import { Subscription } from 'rxjs/Subscription';
 import {DictionaryService} from '../services/dictionary.service';
 import {HistoricalIndexService} from '../services/historical-index.service';
-import { FilterMultiArrayObjectPipe } from '../../common/pipes/filter-multi-array-object.pipe';
+import { FilterMultiArrayObjectPipe } from '../../app-pipes/filter-multi-array-object.pipe';
 import * as _ from 'lodash';
 import * as d3 from 'd3';
 
