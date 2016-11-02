5c4e2d9dedd972682fbe6e15cba9e268930e7fac
dmitriy.ivanovskiy <dmitriy.ivanovskiy@reisystems.com>
2016-11-02 14:32:14 -0400

    initial commit

--- src/app/assistance-listing/pipes/authorization.pipe.ts
+++ src/app/assistance-listing/pipes/authorization.pipe.ts
@@ -15,24 +15,39 @@ export class AuthorizationPipe implements PipeTransform {
       if (oAuthorization.version > 1) {
         title = title.concat(", as amended by ");
       }
-      var type = oAuthorization.authorizationType;
-      switch (type) {
-        case "publiclaw":
-          title = title + (oAuthorization.publicLaw ? ("Public Law " + (oAuthorization.publicLaw.congressCode || "")) : "") + "-" + (oAuthorization.publicLaw ? (oAuthorization.publicLaw.lawNumber || "N/A") : "N/A");
-          break;
-        case "statute":
-          title = title + (oAuthorization.statute ? ("Statute " + (oAuthorization.statute.volume || "")) : "") + "-" + (oAuthorization.statute ? (oAuthorization.statute.page || "N/A") : "N/A");
-          break;
-        case "usc":
-          title = title + (oAuthorization.USC ? (oAuthorization.USC.title || "") : "") + " US Code " + (oAuthorization.USC ? (oAuthorization.USC.section || "") : "");
-          break;
-        case "act":
-          title = title + (oAuthorization.act ? (oAuthorization.act.description || "") : "") + ", Title " + (oAuthorization.act ? (oAuthorization.act.title || "N/A") : "N/A") + ", Part " + (oAuthorization.act ? (oAuthorization.act.part || "N/A") : "N/A") + ", Section " + (oAuthorization.act ? (oAuthorization.act.section || "N/A") : "N/A");
-          break;
-        case "eo":
-          title = title + "Executive Order - " + ((oAuthorization.executiveOrder && oAuthorization.executiveOrder.title) || "N/A");
-          break;
+      if (oAuthorization.act != null){
+        title = title + (oAuthorization.act ? (oAuthorization.act.description || "") : "") + ", Title " + (oAuthorization.act ? (oAuthorization.act.title || "N/A") : "N/A") + ", Part " + (oAuthorization.act ? (oAuthorization.act.part || "N/A") : "N/A") + ", Section " + (oAuthorization.act ? (oAuthorization.act.section || "N/A") : "N/A") + ", ";
       }
+      if (oAuthorization.executiveOrder != null){
+        title = title + "Executive Order - " + ( oAuthorization.executiveOrder ? oAuthorization.executiveOrder.description : "N/A") + ", ";
+      }
+      if (oAuthorization.publicLaw != null){
+        title = title + (oAuthorization.publicLaw ? ("Public Law " + (oAuthorization.publicLaw.congressCode || "")) : "") + "-" + (oAuthorization.publicLaw ? (oAuthorization.publicLaw.lawNumber || "N/A") : "N/A") + ", ";
+      }
+      if (oAuthorization.statute != null){
+        title = title + (oAuthorization.statute ? ("Statute " + (oAuthorization.statute.volume || "")) : "") + "-" + (oAuthorization.statute ? (oAuthorization.statute.page || "N/A") : "N/A") + ", ";
+      }
+      if (oAuthorization.USC != null){
+        title = title + (oAuthorization.USC ? (oAuthorization.USC.title || "") : "") + " US Code " + (oAuthorization.USC ? (oAuthorization.USC.section || "") : "") + ", ";
+      }
+      // var type = oAuthorization.authorizationType;
+      // switch (type) {
+      //   case "publiclaw":
+      //     title = title + (oAuthorization.publicLaw ? ("Public Law " + (oAuthorization.publicLaw.congressCode || "")) : "") + "-" + (oAuthorization.publicLaw ? (oAuthorization.publicLaw.lawNumber || "N/A") : "N/A");
+      //     break;
+      //   case "statute":
+      //     title = title + (oAuthorization.statute ? ("Statute " + (oAuthorization.statute.volume || "")) : "") + "-" + (oAuthorization.statute ? (oAuthorization.statute.page || "N/A") : "N/A");
+      //     break;
+      //   case "usc":
+      //     title = title + (oAuthorization.USC ? (oAuthorization.USC.title || "") : "") + " US Code " + (oAuthorization.USC ? (oAuthorization.USC.section || "") : "");
+      //     break;
+      //   case "act":
+      //     title = title + (oAuthorization.act ? (oAuthorization.act.description || "") : "") + ", Title " + (oAuthorization.act ? (oAuthorization.act.title || "N/A") : "N/A") + ", Part " + (oAuthorization.act ? (oAuthorization.act.part || "N/A") : "N/A") + ", Section " + (oAuthorization.act ? (oAuthorization.act.section || "N/A") : "N/A");
+      //     break;
+      //   case "eo":
+      //     title = title + "Executive Order - " + ((oAuthorization.executiveOrder && oAuthorization.executiveOrder.title) || "N/A");
+      //     break;
+      // }
     });
     return title;
   }
