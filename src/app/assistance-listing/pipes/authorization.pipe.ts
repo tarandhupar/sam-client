7eb46f8928fac1593080eeb7efb2d05b9a732e28
dmitriy.ivanovskiy <dmitriy.ivanovskiy@reisystems.com>
2016-11-02 18:03:33 -0400

    SAM-772

--- src/app/assistance-listing/pipes/authorization.pipe.ts
+++ src/app/assistance-listing/pipes/authorization.pipe.ts
@@ -16,38 +16,20 @@ export class AuthorizationPipe implements PipeTransform {
         title = title.concat(", as amended by ");
       }
       if (oAuthorization.act != null){
-        title = title + (oAuthorization.act ? (oAuthorization.act.description || "") : "") + ", Title " + (oAuthorization.act ? (oAuthorization.act.title || "N/A") : "N/A") + ", Part " + (oAuthorization.act ? (oAuthorization.act.part || "N/A") : "N/A") + ", Section " + (oAuthorization.act ? (oAuthorization.act.section || "N/A") : "N/A") + ", ";
+        title = title + (oAuthorization.act ? (oAuthorization.act.description || "") : "") + ", Title " + (oAuthorization.act ? (oAuthorization.act.title || "N/A") : "N/A") + ", Part " + (oAuthorization.act ? (oAuthorization.act.part || "N/A") : "N/A") + ", Section " + (oAuthorization.act ? (oAuthorization.act.section || "N/A") : "N/A") + "\n";
       }
       if (oAuthorization.executiveOrder != null){
-        title = title + "Executive Order - " + ( oAuthorization.executiveOrder ? oAuthorization.executiveOrder.description : "N/A") + ", ";
+        title = title + "Executive Order - " + ( oAuthorization.executiveOrder ? oAuthorization.executiveOrder.description : "N/A") + "\n";
       }
       if (oAuthorization.publicLaw != null){
-        title = title + (oAuthorization.publicLaw ? ("Public Law " + (oAuthorization.publicLaw.congressCode || "")) : "") + "- " + (oAuthorization.publicLaw ? (oAuthorization.publicLaw.lawNumber || "N/A") : "N/A") + ", ";
+        title = title + (oAuthorization.publicLaw ? ("Public Law " + (oAuthorization.publicLaw.congressCode || "")) : "") + "- " + (oAuthorization.publicLaw ? (oAuthorization.publicLaw.number || "N/A") : "N/A") + "\n";
       }
       if (oAuthorization.statute != null){
-        title = title + (oAuthorization.statute ? ("Statute " + (oAuthorization.statute.volume || "")) : "") + "-" + (oAuthorization.statute ? (oAuthorization.statute.page || "N/A") : "N/A") + ", ";
+        title = title + (oAuthorization.statute ? ("Statute " + (oAuthorization.statute.volume || "")) : "") + "-" + (oAuthorization.statute ? (oAuthorization.statute.page || "N/A") : "N/A") + "\n";
       }
       if (oAuthorization.USC != null){
-        title = title + (oAuthorization.USC ? (oAuthorization.USC.title || "") : "") + " US Code " + (oAuthorization.USC ? (oAuthorization.USC.section || "") : "") + ", ";
+        title = title + (oAuthorization.USC ? (oAuthorization.USC.title || "") : "") + " US Code " + (oAuthorization.USC ? (oAuthorization.USC.section || "") : "") + "\n";
       }
-      // var type = oAuthorization.authorizationType;
-      // switch (type) {
-      //   case "publiclaw":
-      //     title = title + (oAuthorization.publicLaw ? ("Public Law " + (oAuthorization.publicLaw.congressCode || "")) : "") + "-" + (oAuthorization.publicLaw ? (oAuthorization.publicLaw.lawNumber || "N/A") : "N/A");
-      //     break;
-      //   case "statute":
-      //     title = title + (oAuthorization.statute ? ("Statute " + (oAuthorization.statute.volume || "")) : "") + "-" + (oAuthorization.statute ? (oAuthorization.statute.page || "N/A") : "N/A");
-      //     break;
-      //   case "usc":
-      //     title = title + (oAuthorization.USC ? (oAuthorization.USC.title || "") : "") + " US Code " + (oAuthorization.USC ? (oAuthorization.USC.section || "") : "");
-      //     break;
-      //   case "act":
-      //     title = title + (oAuthorization.act ? (oAuthorization.act.description || "") : "") + ", Title " + (oAuthorization.act ? (oAuthorization.act.title || "N/A") : "N/A") + ", Part " + (oAuthorization.act ? (oAuthorization.act.part || "N/A") : "N/A") + ", Section " + (oAuthorization.act ? (oAuthorization.act.section || "N/A") : "N/A");
-      //     break;
-      //   case "eo":
-      //     title = title + "Executive Order - " + ((oAuthorization.executiveOrder && oAuthorization.executiveOrder.title) || "N/A");
-      //     break;
-      // }
     });
     return title;
   }
