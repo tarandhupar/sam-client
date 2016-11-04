ad21ca3dba9e2e46b74aad57b1a9972e33d20748
Josh Bruce <josh@8foldproductivity.com>
2016-11-04 13:44:01 -0400

    Extractions

--- src/app/assistance-listing/pipes/authorization.pipe.ts
+++ src/app/assistance-listing/pipes/authorization.pipe.ts
@@ -3,90 +3,121 @@ import * as _ from 'lodash';
 
 @Pipe({name: 'authorization'})
 export class AuthorizationPipe implements PipeTransform {
+  sortBy: string = 'version';
+
+  // TODO: Promote to its own file or pipe to make reusable labels.
+  titleString: string = 'Title';
+  partString: string = 'Part';
+  sectionString: string = 'Section';
+  executiveOrderString: string = 'Executive Order';
+  pulicLawString: string = 'Public Law';
+  statuteString: string = 'Statute';
+  usCodeString: string = 'US Code';
+  asAmendedByString: string = 'as amended by';
+
   transform(authorizations, args:string[]):any {
 
     //make sure sorted by version number
-    authorizations = _.sortBy(authorizations, 'version');
+    authorizations = _.sortBy(authorizations, this.sortBy);
 
     // TODO, review implementation of string building of the the authorizations
     var title = [];
     _.forEach(authorizations, authorization => {
 
-      // TODO: No test in the scpec for this
-      // could be its own method call
+      // TODO: No test in the scpec for this...??
+      // TODO: Would prefer the conditionals before the call be in the method;
+      //       however, title.push is creating empty indeces within title
       if (authorization.version > 1) {
-        title.push("as amended by ");
+        title.push(this.getAmendedBy(authorization));
       }
 
-      // could be its own method call
       if (authorization.act != null) {
-        var actText = [];
-        if (authorization.act.description) {
-          actText.push(authorization.act.description);
-        }
-        if (authorization.act.title) {
-          actText.push('Title ' + authorization.act.title);
-        }
-        if (authorization.act.part) {
-          actText.push('Part ' + authorization.act.part);
-        }
-        if (authorization.act.section) {
-          actText.push('Section ' + authorization.act.section);
-        }
-        let actString = actText.join(', ');
-        title.push(actString);
+        title.push(this.getAuthorizationAct(authorization));
       }
 
-      // could be its own method
       if (authorization.executiveOrder != null && authorization.executiveOrder.description) {
-        title.push('Executive Order - ' + authorization.executiveOrder.description);
+        title.push(this.getExecutiveOrder(authorization));
       }
 
-      // could be its own method
       if (authorization.publicLaw != null) {
-        var lawText = [];
-        if (authorization.publicLaw.congressCode) {
-          lawText.push('Public Law ' + authorization.publicLaw.congressCode);
-        }
-        if (authorization.publicLaw.number) {
-          lawText.push(authorization.publicLaw.number);
-        }
-        let lawString = lawText.join('-');
-        title.push(lawString);
+        title.push(this.getPublicLaw(authorization));
       }
 
-      // could be its own method
       if (authorization.statute != null){
-        var statuteText = [];
-        if (authorization.statute.volume) {
-          statuteText.push('Statute ' + authorization.statute.volume);
-        }
-        if (authorization.statute.page) {
-          statuteText.push(authorization.statute.page);
-        }
-        let statuteString = statuteText.join('-');
-        title.push(statuteText);
+        title.push(this.getStatute(authorization));
       }
 
-      // could be its own method
       if (authorization.USC != null){
-        var authorizationText = [];
-        if (authorization.USC.title) {
-          authorizationText.push(authorization.USC.title);
-        }
-        if (authorization.USC.section) {
-          authorizationText.push('US Code ' + authorization.USC.section);
-        }
-        let authorizationString = authorizationText.join(' ');
-        title.push(authorizationString);
+        title.push(this.getUSCode(authorization));
       }
     });
     title = title.join(', ');
-    // title = title.slice(0, -2);
     return title;
   }
 
-  getAmendedBy(authorization) {
+  private getAmendedBy(authorization) {
+    return this.asAmendedByString;
+  }
 
+  private getAuthorizationAct(authorization) {
+    var actText = [];
+    if (authorization.act.description) {
+      actText.push(authorization.act.description);
+    }
+    if (authorization.act.title) {
+      actText.push(this.titleString + ' ' + authorization.act.title);
+    }
+    if (authorization.act.part) {
+      actText.push(this.partString + ' ' + authorization.act.part);
+    }
+    if (authorization.act.section) {
+      actText.push(this.sectionString + ' ' + authorization.act.section);
+    }
+    let actString = actText.join(', ');
+    return actString;
+  }
+
+  private getExecutiveOrder(authorization)
+  {
+    return this.executiveOrderString + ' - ' + authorization.executiveOrder.description;
+  }
+
+  private getPublicLaw(authorization)
+  {
+    var lawText = [];
+    if (authorization.publicLaw.congressCode) {
+      lawText.push(this.pulicLawString + ' ' + authorization.publicLaw.congressCode);
+    }
+    if (authorization.publicLaw.number) {
+      lawText.push(authorization.publicLaw.number);
+    }
+    let lawString = lawText.join('-');
+    return lawString;
+  }
+
+  private getStatute(authorization)
+  {
+    var statuteText = [];
+    if (authorization.statute.volume) {
+      statuteText.push(this.statuteString + ' ' + authorization.statute.volume);
+    }
+    if (authorization.statute.page) {
+      statuteText.push(authorization.statute.page);
+    }
+    let statuteString = statuteText.join('-');
+    return statuteText;
+  }
+
+  private getUSCode(authorization)
+  {
+    var authorizationText = [];
+    if (authorization.USC.title) {
+      authorizationText.push(authorization.USC.title);
+    }
+    if (authorization.USC.section) {
+      authorizationText.push(this.usCodeString + ' ' + authorization.USC.section);
+    }
+    let authorizationString = authorizationText.join(' ');
+    return authorizationString;
   }
 }
