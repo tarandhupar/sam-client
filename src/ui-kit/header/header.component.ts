f66dadec8e97f2c8bacc02093c5d80867228c34f
Justin Babbs <justin.babbs@octoconsulting.com>
2016-11-06 21:42:32 -0500

    add the images and fonts from samwds to our projects
    bug fix: the blue background would sometimes not appear around the header

--- src/ui-kit/header/header.component.ts
+++ src/ui-kit/header/header.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'samHeader',
-  template:`<header class="sam-header">
+  template:`<header class="sam-header clearfix">
               <nav class="" aria-label="Main navigation">
                 <div class="usa-width-one-fourth align-top marginCenter centered">
                     <a class="marginCenter">
-                      <img class="marginCenter header-logo-img" src="../../../assets/img/sam_hat_logo.jpg">
+                      <img class="marginCenter header-logo-img" src="assets/img/sam_hat_logo.jpg">
                     </a>
-
                 </div>
               </nav>
             </header>`,
