c7e51ff8b398eb6d326567bb37ddb95526d7a293
Justin Babbs <justin.babbs@octoconsulting.com>
2016-11-08 14:46:14 -0500

    cleanup header css

--- src/ui-kit/header/header.component.ts
+++ src/ui-kit/header/header.component.ts
@@ -2,14 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'samHeader',
-  template:`<header class="sam-header clearfix">
-              <nav class="" aria-label="Main navigation">
-                <div class="usa-width-one-fourth align-top marginCenter centered">
-                    <a class="marginCenter">
-                      <img class="marginCenter header-logo-img" src="assets/img/sam_hat_logo.jpg">
-                    </a>
-                </div>
-              </nav>
+  template:`<header>
+              <img src="assets/img/sam_hat_logo.jpg">
             </header>`,
   styleUrls: ['header.scss']
 })
