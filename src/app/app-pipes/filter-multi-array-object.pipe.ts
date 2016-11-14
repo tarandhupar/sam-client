94a6132a22f9b63db976f59550bba7db67a96a3f
Kevin Song <kevin.song@reisystems.com>
2016-11-14 16:36:41 -0500

    Change FilterMultiArrayObjectPipe to use existing loadash methods

--- src/app/app-pipes/filter-multi-array-object.pipe.ts
+++ src/app/app-pipes/filter-multi-array-object.pipe.ts
@@ -1,11 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as _ from 'lodash';
 
 /**
  * Function to return an array of matched data by an ID
  *
  * This function accept flat array of object of data or hierarchical array of objects
  *
- * @param Array value | ["2"]
+ * @param Array values | ["2"]
  * @param Array data | [{"id": "1", "value":"foo", "children": [{"id":"11", "value": "foo 1.1"}], {"id": "2", value: "bar"} }]
  * @param String fieldName | "id" ; the field name in @param aData to search @param aValue against
  * @param isNested Boolean | true/false ; has @param aData children field to search data against
@@ -15,42 +16,34 @@ import { Pipe, PipeTransform } from '@angular/core';
  */
 @Pipe({name: 'filterMultiArrayObject'})
 export class FilterMultiArrayObjectPipe implements PipeTransform {
-  transform(value: any[], data: any[], fieldName: string, isNested: boolean, nestedFieldName: string): any[] {
+  transform(values: any[], data: any[], fieldName: string, isNested: boolean, nestedFieldName: string): any[] {
 
     let tmpArray = data;
-    let results: any[] = [];
 
     // nested array of objects
     if (isNested === true) {
       tmpArray = this.flattenMultiArrayObject(data, nestedFieldName);
     }
 
-    value.forEach(value => {
-      let tmp = tmpArray.filter(item => {
-        if (value === item[fieldName]) {
-          return item;
-        }
+    // for each search value, filter the data to get a list of items that match the search value
+    // then combine all lists of results together and flatten them, taking only unique results
+    return _.chain(_.flatMap(values, value => {
+      return _.filter(tmpArray, item => {
+        return value === item[fieldName];
       });
-
-      results = results.concat(tmp);
-    });
-
-    return results;
+    })).uniq();
   }
 
   private flattenMultiArrayObject(obj: any[], nestedFieldName: string): any[] {
-    let accumulator = [];
-
-    // loop through current level of array
-    obj.forEach(item => {
-      // base case: return current level if not nested
-      accumulator = accumulator.concat(item);
+    // for each item in the object, add all its children and itself to the accumulator array
+    return _.reduce(obj, (accumulator, item) => {
       // recursive case: flatten nested array if it exists
       if (item[nestedFieldName] && item[nestedFieldName] instanceof Array) {
         accumulator = accumulator.concat(this.flattenMultiArrayObject(item[nestedFieldName], nestedFieldName));
       }
-    });
 
-    return accumulator;
+      // base case: return current level if not nested
+      return accumulator.concat(item);
+    }, []); // the accumulator begins as empty []
   }
 }
