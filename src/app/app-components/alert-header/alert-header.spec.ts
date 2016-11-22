83c975e900546b483c5528603ce193fba36e96c8
Justin Babbs <justin.babbs@octoconsulting.com>
2016-11-21 21:12:24 -0500

    fix unit tests

--- src/app/app-components/alert-header/alert-header.spec.ts
+++ src/app/app-components/alert-header/alert-header.spec.ts
@@ -9,36 +9,42 @@ import {SamUIKitModule} from 'ui-kit';
 import {RouterTestingModule} from "@angular/router/testing";
 
 let error = {
-  "title" : "The is an error", 
-  "summary" : "The systems will be down for a while", 
-  "category" : "outages", 
-  "description" : "This is the description", 
-  "severity" : "ERROR",
-  "begins" : "2016-11-01T20:03:09Z",
-  "expires" : "2016-11-01T20:03:09Z",
-  "published" : "2016-11-01T20:03:09Z" 
+  content: {
+    "title" : "The is an error", 
+    "summary" : "The systems will be down for a while", 
+    "category" : "outages", 
+    "description" : "This is the description", 
+    "severity" : "ERROR",
+    "begins" : "2016-11-01T20:03:09Z",
+    "expires" : "2016-11-01T20:03:09Z",
+    "published" : "2016-11-01T20:03:09Z" 
+  }
 };
 
 let warning = {
-  "title" : "The is an warning",
-  "summary" : "The systems will be slow for a while",
-  "category" : "outages",
-  "description" : "This is the description",
-  "severity" : "WARNING",
-  "begins" : "2016-11-01T20:03:09Z",
-  "expires" : "2016-11-01T20:03:09Z",
-  "published" : "2016-11-01T20:03:09Z"
+  content: {
+    "title": "The is an warning",
+    "summary": "The systems will be slow for a while",
+    "category": "outages",
+    "description": "This is the description",
+    "severity": "WARNING",
+    "begins": "2016-11-01T20:03:09Z",
+    "expires": "2016-11-01T20:03:09Z",
+    "published": "2016-11-01T20:03:09Z"
+  }
 };
 
 let info = {
-  "title" : "The is information",
-  "summary" : "The systems will have additional features tomorrow",
-  "category" : "features",
-  "description" : "This is the description",
-  "severity" : "INFO",
-  "begins" : "2016-11-01T20:03:09Z",
-  "expires" : "2016-11-01T20:03:09Z",
-  "published" : "2016-11-01T20:03:09Z"
+  content: {
+    "title": "The is information",
+    "summary": "The systems will have additional features tomorrow",
+    "category": "features",
+    "description": "This is the description",
+    "severity": "INFO",
+    "begins": "2016-11-01T20:03:09Z",
+    "expires": "2016-11-01T20:03:09Z",
+    "published": "2016-11-01T20:03:09Z"
+  }
 };
 
 let noAlerts = Observable.of([]);
