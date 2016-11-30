99fe788cdde95af3caaf2d15b029e891f171500d
Carryace <sunsm1991@gmail.com>
2016-11-30 14:12:27 -0500

    Add in reference library with test cases

---
+++ src/app/Help/sections/reference-library/reference-library.spec.ts
@@ -0,0 +1,66 @@
+import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+
+// Load the implementations that should be tested
+import { ReferenceLibraryComponent } from "./reference-library.component";
+import { HelpModule } from "../../help.module"
+
+describe('Reference Library Component in Help page', () => {
+  let component:ReferenceLibraryComponent;
+  let fixture:any;
+
+  let federalItemConfig = {
+    title:"Data Element Repository",
+    detail:"Details for Data Element Repository: ",
+  };
+  let contractItemConfig = {
+    title:"Federal Acquisition Regulation",
+    detail:"Details for Data Element Repository: ",
+  };
+
+  // provide our implementations or mocks to the dependency injector
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ReferenceLibraryComponent],
+      imports: [HelpModule,RouterTestingModule]
+
+    });
+    fixture = TestBed.createComponent(ReferenceLibraryComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should compile', () => {
+    fixture.detectChanges();
+    expect(true).toBe(true);
+  });
+
+  it('should contain featured references section and additional references section', ()=>{
+    fixture.detectChanges();
+    let featuredRef = fixture.debugElement.query(By.css('#featuredReference'));
+    let additionalRef = fixture.debugElement.query(By.css('#additionalReference'));
+    expect(featuredRef).toBeDefined();
+    expect(featuredRef.nativeElement.innerHTML).toBe("Featured References");
+    expect(additionalRef).toBeDefined();
+    expect(additionalRef.nativeElement.innerHTML).toBe("Additional References");
+  });
+
+  it('should toggle federal assistance detail when clicking on federal assistance image, link or icon', ()=>{
+    fixture.detectChanges();
+    expect(component.detailObj.Federal.showDetail).toBe(false);
+    component.selectDetail(federalItemConfig,"Federal");
+    expect(component.detailObj.Federal.showDetail).toBe(true);
+    component.selectDetail(federalItemConfig,"Federal");
+    expect(component.detailObj.Federal.showDetail).toBe(false);
+  });
+
+  it('should toggle contract detail when clicking on contract image, link or icon', ()=>{
+    fixture.detectChanges();
+    expect(component.detailObj.Contract.showDetail).toBe(false);
+    component.selectDetail(contractItemConfig,"Contract");
+    expect(component.detailObj.Contract.showDetail).toBe(true);
+    component.selectDetail(contractItemConfig,"Contract");
+    expect(component.detailObj.Contract.showDetail).toBe(false);
+  });
+
+});
