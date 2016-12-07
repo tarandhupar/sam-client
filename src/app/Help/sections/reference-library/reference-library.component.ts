dd7dda4b0231c410bedfe9073ab1dd8820b92507
Carryace <sunsm1991@gmail.com>
2016-12-07 11:28:13 -0500

    Add in external links and implement latest wireframe

--- src/app/Help/sections/reference-library/reference-library.component.ts
+++ src/app/Help/sections/reference-library/reference-library.component.ts
@@ -14,13 +14,11 @@ export class ReferenceLibraryComponent {
   detailObj: any = {
     Federal:{
       showDetail: false,
-      title: "",
-      detail: ""
+      item: {}
     },
     Contract:{
       showDetail: false,
-      title: "",
-      detail: ""
+      item: {}
     }
   };
 
@@ -28,55 +26,67 @@ export class ReferenceLibraryComponent {
     Federal: [
         {
           title:"Uniform Grant Guidance (2 CFR 200)",
-          detail:"Details for Uniform Grant Guidance: ",
+          detail:"Details for Uniform Grant Guidance: "+this.detailLipsum,
+          link:"View the e-CFR",
+          url:"fakeUrl"
         },
         {
           title:"Grants.gov Learning Center",
-          detail:"Details for Grants.gov Learning Center: ",
+          detail:"Details for Grants.gov Learning Center: "+this.detailLipsum,
+          link:"View the Grants.gov",
+          url:"http://www.grants.gov/web/grants/learn-grants.html"
         },
         {
           title:"Data Element Repository",
-          detail:"Details for Data Element Repository: ",
+          detail:"Details for Data Element Repository: "+this.detailLipsum,
+          link:"View the DER",
+          url:"fakeUrl"
         }
       ],
     Contract:[
         {
           title:"Federal Acquisition Regulation",
-          detail:"Details for Data Element Repository: ",
+          detail:"Details for Federal Acquisition Regulation: "+this.detailLipsum,
+          link:"View the FAR",
+          url:"fakeUrl"
         },
         {
           title:"Contracts Reference Example1",
-          detail:"Details for Contracts Reference Example1: ",
+          detail:"Details for Contracts Reference Example1: "+this.detailLipsum,
+          link:"View example1",
+          url:"hfakeUrl"
         },
         {
           title:"Contracts Reference Example2",
-          detail:"Details for Contracts Reference Example2: ",
+          detail:"Details for Contracts Reference Example2: "+this.detailLipsum,
+          link:"View example2",
+          url:"fakeUrl"
         }
     ]
   };
 
   constructor() { }
 
-  selectDetail(item, type){
-    if(this.detailObj[type].title === item.title){
+  selectDetail(item, type, event){
+    if(this.detailObj[type].item.title === item.title){
       this.detailObj[type].showDetail = false;
-      this.detailObj[type].title = "";
+      this.detailObj[type].item = {};
     }else{
       this.detailObj[type].showDetail = true;
-      this.detailObj[type].title = item.title;
-      this.detailObj[type].detail = item.detail + this.detailLipsum;
+      this.detailObj[type].item = item;
     }
+    event.stopPropagation();
   }
 
-  private getItemClass(item, type): string{
-    if(this.detailObj[type].title !== item.title){
+  private getItemClass(item, type){
+    if(this.detailObj[type].item.title !== item.title){
       return "fa-plus";
     }
     return "fa-minus";
   }
 
   private getTriClass(index, type): string{
-    if(this.detailObj[type].title === this.data[type][index].title){
+    if(this.detailObj[type].item.title === this.data[type][index].title){
       return "tri-down";
     }
     return "no-tri-down";
@@ -84,6 +94,20 @@ export class ReferenceLibraryComponent {
 
   private closeReferenceDetail(type){
     this.detailObj[type].showDetail = false;
-    this.detailObj[type].title = "";
+    this.detailObj[type].item = {};
+  }
+
+  private getLayerClass(index,type): string{
+    if(this.detailObj[type].item.title === this.data[type][index].title){
+      return "reference-image-layer-select";
+    }
+    return "reference-image-layer-unselect";
+  }
+
+  private getBorderClass(index,type): string{
+    if(this.detailObj[type].item.title === this.data[type][index].title){
+      return "item-border-select";
+    }
+    return "item-border-unselect";
   }
 }
