7e15440a835ed416f85fdc0e49cf31fb0d85f272
Justin Babbs <justin.babbs@octoconsulting.com>
2016-12-13 09:52:19 -0500

    use alert url variable instead of hardcode value

--- src/ui-kit/footer/footer.component.ts
+++ src/ui-kit/footer/footer.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { globals } from '../../app/globals.ts';
+import { SYSTEM_ALERTS_PAGE_PATH } from "../../app/alerts/alerts.route";
+
 
 @Component({
   selector: 'samFooter',
   templateUrl:'footer.template.html',
 })
 export class SamFooterComponent {
+  alertsUrl: string = SYSTEM_ALERTS_PAGE_PATH;
 
   constructor() {
   }
