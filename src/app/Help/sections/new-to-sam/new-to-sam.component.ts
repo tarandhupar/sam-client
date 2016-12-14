f8eea9875b817f62ab21f30d7e95939aae2d1d94
nathansun <nathan.sun@octoconsulting.com>
2016-12-14 16:14:19 -0500

    wip: new to sam.gov page

--- src/app/Help/sections/new-to-sam/new-to-sam.component.ts
+++ src/app/Help/sections/new-to-sam/new-to-sam.component.ts
@@ -6,6 +6,43 @@ import { Component } from '@angular/core';
 })
 export class NewToSamComponent {
 
+  detailLipsum:string = `Omnes inermis ius at, ad assum constituto referrentur eam. 
+    Regione deserunt no vis, in his sale aeque. Saepe virtute impedit no nec, elitr 
+    decore antiopam cu usu, sit Id labores vivendum vim.`;
+
+  private data:any = [
+    {
+      title:"Lipsum text title1",
+      detail:"Detail Lipsum text1: "+this.detailLipsum,
+      link:"Lipsum link",
+      url:"fakeUrl"
+    },
+    {
+      title:"Lipsum text title2",
+      detail:"Detail Lipsum text2: "+this.detailLipsum,
+      link:"Lipsum link",
+      url:"fakeUrl"
+    },
+    {
+      title:"Lipsum text title3",
+      detail:"Detail Lipsum text3: "+this.detailLipsum,
+      link:"Lipsum link",
+      url:"fakeUrl"
+    }
+  ];
+
+  private showImageLibrary: boolean = false;
+
   constructor() { }
+
+  openImageLibrary(){
+    this.showImageLibrary = true;
+  }
+
+  closeImageLibrary(){
+    this.showImageLibrary = false;
+  }
+
+
 
 }
