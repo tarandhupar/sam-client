5499580452e067dda49b4000148d8465eae9dbd7
nathansun <nathan.sun@octoconsulting.com>
2016-12-16 11:03:14 -0500

    Add fake data to show the process

--- src/app/Help/sections/new-to-sam/new-to-sam.component.ts
+++ src/app/Help/sections/new-to-sam/new-to-sam.component.ts
@@ -10,37 +10,57 @@ export class NewToSamComponent {
     Regione deserunt no vis, in his sale aeque. Saepe virtute impedit no nec, elitr 
     decore antiopam cu usu, sit Id labores vivendum vim.`;
 
-  private data:any = [
+  //Lipsum data for each image icon
+  private imageGroupdata:any = [
     {
       title:"Lipsum text title1",
       detail:"Detail Lipsum text1: "+this.detailLipsum,
       link:"Lipsum link",
-      url:"fakeUrl"
+      url:"help"
     },
     {
       title:"Lipsum text title2",
       detail:"Detail Lipsum text2: "+this.detailLipsum,
       link:"Lipsum link",
-      url:"fakeUrl"
+      url:"help"
     },
     {
       title:"Lipsum text title3",
       detail:"Detail Lipsum text3: "+this.detailLipsum,
       link:"Lipsum link",
-      url:"fakeUrl"
+      url:"help"
     }
   ];
+
+  private imageGroupIndexData:any = {
+    area1:[[0,2,1],[0,1,2],[1,2,0]],
+    area2:[[0,1,2],[0,2,1],[1,0,2]],
+    area3:[[1,2,0],[2,1,0],[0,2,1]],
+    area4:[[0,2,1],[0,2,1],[1,0,2]],
+    area5:[[2,1,0],[0,1,2],[2,0,1]],
+  };
+
+  private curImageGroup:any;
 
   showImageLibrary: boolean = false;
 
   constructor() { }
 
-  openImageLibrary(){
+  openImageLibrary(type){
     this.showImageLibrary = true;
+    this.curImageGroup = [];
+    for(let indexList of this.imageGroupIndexData[type]){
+      let tempDataList: any = [];
+      for(let indexItem of indexList){
+        tempDataList.push(this.imageGroupdata[indexItem]);
+      }
+      this.curImageGroup.push(tempDataList);
+    }
   }
 
   closeImageLibrary(){
     this.showImageLibrary = false;
+    this.curImageGroup = {};
   }
 
 
