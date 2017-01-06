5923a45d9b1cd26a93e9fa47d97ccd4f8fdf88f3
Justin Babbs <justin.babbs@octoconsulting.com>
2017-01-06 12:30:29 -0500

    wip

--- src/app/alerts/alert-item/alert-item.component.ts
+++ src/app/alerts/alert-item/alert-item.component.ts
@@ -1,5 +1,6 @@
 import {Input, Output, Component, EventEmitter} from '@angular/core';
 import {Alert} from "../alert.model";
+import * as moment from 'moment/moment';
 
 @Component({
   selector: 'alert-item',
@@ -13,5 +14,21 @@ export class AlertItemComponent {
 
   onEditClick() {
     this.edit.emit(this.alert);
+  }
+
+  formatDate(dateString) {
+    if (dateString) {
+      return moment(dateString).format('MMM DD, YYYY');
+    } else {
+      return '--';
+    }
+  }
+
+  publishedDate() {
+    return this.formatDate(this.alert.publishedDate());
+  }
+
+  endDate() {
+    return this.formatDate(this.alert.endDate());
   }
 }
