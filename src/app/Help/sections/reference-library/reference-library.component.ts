a42cc4f437779e623ef827480541c1d62c9ce545
nathansun <nathan.sun@octoconsulting.com>
2017-01-10 16:18:23 -0500

    Enable writing content for reference library

--- src/app/Help/sections/reference-library/reference-library.component.ts
+++ src/app/Help/sections/reference-library/reference-library.component.ts
@@ -7,30 +7,25 @@ import { globals } from '../../../globals';
 })
 export class ReferenceLibraryComponent {
 
-  detailLipsum:string = `Omnes inermis ius at, ad assum constituto referrentur eam. 
-    Regione deserunt no vis, in his sale aeque. Saepe virtute impedit no nec, elitr 
-    decore antiopam cu usu, sit Id labores vivendum vim.`;
-
-
   private data: any={
     Federal: [
         {
           title:"Grants.gov Learning Center",
-          detail:"Details for Grants.gov Learning Center: "+this.detailLipsum,
+          detail:"Here you can learn and navigate the step-by-step process of the grant lifecycle, from the online application phase, pre-award phase, and post award phase.",
           link:"View Grants.gov",
           url:"http://www.grants.gov/web/grants/learn-grants.html",
           img:"src/assets/img/logos/Grants-Gov-Logo.png"
         },
         {
           title:"Benefit Finder",
-          detail:"Benefit Finder: "+this.detailLipsum,
+          detail:"An easy-to-use online tool to find government benefit information and your eligibility. ",
           link:"View Benefits.gov",
           url:"https://www.benefits.gov/benefits/benefit-finder#benefits&qc=cat_1",
           img:"src/assets/img/logos/benefits-logo.png"
         },
         {
           title:"Government Benefits, Grants, and Loans",
-          detail:"Details for Government Benefits, Grants, and Loans: "+this.detailLipsum,
+          detail:"All you need to know about affordable housing, grants, and loans are located under this comprehensive website. Go here to learn more. ",
           link:"View USA.gov",
           url:"https://www.usa.gov/benefits-grants-loans",
           img:"src/assets/img/logos/usagov_logo_hi_res.png"
@@ -39,21 +34,21 @@ export class ReferenceLibraryComponent {
     Contract:[
         {
           title:"Federal Acquisition Regulation",
-          detail:"Details for Federal Acquisition Regulation: "+this.detailLipsum,
+          detail:"The FAR is your primary resource documenting the policies and procedures that govern acquisitions by all executive agencies of the U.S. federal government.  It provides for coordination, simplicity, and uniformity in the Federal acquisition process.  You can quickly access any section of the Federal Acquisition Regulation electronically by using the online table of contents on this page. ",
           link:"View FAR",
           url:"https://www.acquisition.gov/?q=browsefar",
           img:"src/assets/img/placeholder.jpg"
         },
         {
           title:"Compliance Assistance",
-          detail:"Details for Compliance Assistance: "+this.detailLipsum,
+          detail:"Obtain assistance on government contracts, include wage laws, workers rights, and more. ",
           link:"View Compliance Assistance",
           url:"https://www.dol.gov/whd/govcontracts/",
           img:"src/assets/img/placeholder.jpg"
         },
         {
           title:"Request DUNS Number",
-          detail:"Details for Request DUNS Number: "+this.detailLipsum,
+          detail:"A Duns & Bradstreet (DUNS) Number is a unique 9-digit identification number assigned to businesses worldwide. Go here to look up a DUNS Number or request a new one. ",
           link:"View DUNS Number",
           url:"http://fedgov.dnb.com/webform/index.jsp",
           img:"src/assets/img/placeholder.jpg"
@@ -63,7 +58,4 @@ export class ReferenceLibraryComponent {
 
   constructor() { }
 
-  private linkToggle():boolean{
-    return globals.showOptional;
-  }
 }
