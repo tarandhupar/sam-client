3d4db0bcb551e2cf01bb036f0125c3954e09f29a
Kevin Chiu <chiu_kevin@bah.com>
2017-01-11 16:40:41 -0500

    - PasswordComponent bootstrap

---
+++ src/app/authentication/profile/password/password.component.ts
@@ -0,0 +1,57 @@
+import * as _ from 'lodash';
+
+import { Component, DoCheck, Input, NgZone, OnInit, OnChanges } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { IAMService } from 'api-kit';
+import { Validators as $Validators } from '../../shared/validators';
+
+import { User } from '../user.interface';
+import { KBA } from '../kba.interface';
+
+@Component({
+  templateUrl: './password.component.html',
+  providers: [
+    IAMService
+  ]
+})
+export class PasswordComponent {
+  public passwordForm: FormGroup;
+
+  constructor(
+    private router: Router,
+    private builder: FormBuilder,
+    private zone: NgZone,
+    private api: IAMService) {}
+
+  ngOnInit() {
+    this.zone.runOutsideAngular(() => {
+      this.checkSession(() => {
+        this.zone.run(() => {
+          this.passwordForm = this.builder.group({
+            userPassword: ['', Validators.required],
+          })
+        });
+      });
+    });
+  }
+
+  checkSession(cb) {
+    if(this.api.iam.isDebug()) {
+      cb();
+    } else {
+      this.api.iam.checkSession(() => {
+        cb();
+      }, () => {
+        this.router.navigate(['/signin']);
+      });
+    }
+  }
+
+  reset() {
+    this.zone.runOutsideAngular(() => {
+      //TODO
+    });
+  }
+};
--- src/app/authentication/profile/profile.routes.ts
+++ src/app/authentication/profile/profile.routes.ts
@@ -1,6 +1,8 @@
 import { DetailsComponent } from './details/details.component.ts';
+import { PasswordComponent } from './password/password.component.ts';
 
 export default [
   { path: '',  redirectTo: 'details' },
-  { path: 'details', component: DetailsComponent }
+  { path: 'details', component: DetailsComponent },
+  { path: 'password', component: PasswordComponent }
 ];
