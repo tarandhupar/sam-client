763698cb968f30266734aafbb2e5c6d3491336b6
Kevin Song <kevin.song@reisystems.com>
2017-01-16 14:40:29 -0500

    [SAM-2297] Make assistance listing routes consistent with other view pages

--- src/app/assistance-listing/assistance-listing.route.ts
+++ src/app/assistance-listing/assistance-listing.route.ts
@@ -2,8 +2,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProgramPage } from './assistance-listing.page';
 
 export const routes: Routes = [
-  { path: 'programs/:id/view', component: ProgramPage },
-  { path: 'programs',  component: ProgramPage },
+  {
+    path: 'programs/:id/view',
+    component: ProgramPage
+  }
 ];
 
 export const routing = RouterModule.forChild(routes);
