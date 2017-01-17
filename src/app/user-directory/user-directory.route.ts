6915830f581ee1506541676a4aee5ff14adb322b
Justin Babbs <justin.babbs@octoconsulting.com>
2017-01-17 13:01:33 -0500

    conditionally show user-directory only in comp. style changes.

--- src/app/user-directory/user-directory.route.ts
+++ src/app/user-directory/user-directory.route.ts
@@ -1,8 +1,10 @@
 import { Routes, RouterModule } from '@angular/router';
 import { UserDirectoryPage } from './user-directory.page';
 
-export const routes: Routes = [
-  { path: 'users',  component: UserDirectoryPage },
-];
+export const routes: Routes = [];
+
+if (SHOW_OPTIONAL === 'true' || ENV === 'development') {
+  routes.unshift({ path: 'users',  component: UserDirectoryPage });
+}
 
 export const routing = RouterModule.forChild(routes);
