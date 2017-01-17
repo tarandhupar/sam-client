c8b87567fc565a4d60f05871e26f6da70131a06e
Kevin Song <kevin.song@reisystems.com>
2017-01-17 15:25:23 -0500

    [SAM-2925] Add comments and unit tests for filesize pipe

--- src/app/opportunity/pipes/filesize.pipe.ts
+++ src/app/opportunity/pipes/filesize.pipe.ts
@@ -2,13 +2,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 import * as filesize from 'filesize';
 
+// Transforms a filesize in bytes to a human readable decimal filesize using SI prefixes
 @Pipe({name: 'filesize'})
 export class FilesizePipe implements PipeTransform {
 
   transform(size: number): string {
-    if(size < 1000) {
-      return "<1 kB";
-    }
+    // Order of operations:
+    // 1. check if size is under 1000 bytes
+    // 2. convert to largest possible unit
+    // 3. round result to the one's digit
+    if(size <= 0) { console.log('invalid file size', size) }
+    if(size < 1000) { return '<1 kB'; }
+
     else {
       return filesize(size, {round: 0, base: 10});
     }
