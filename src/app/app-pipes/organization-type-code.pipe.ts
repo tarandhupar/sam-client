6569f7573018a709da2120ceddf3f5d31927c05e
Michael Kellogg <michael.kellogg@C02RV0RHG8WL.local>
2017-01-18 15:36:42 -0500

    fixed errors with unit testing

--- src/app/app-pipes/organization-type-code.pipe.ts
+++ src/app/app-pipes/organization-type-code.pipe.ts
@@ -10,20 +10,20 @@ export class OrganizationTypeCodePipe implements PipeTransform {
     };
 
     // check if data items exist, if they do return the label and the data value as properties of an object
-    if(typeof data.aacCode != 'undefined' || data.aacCode != null || data.aacCode != ''){
+    if(data.aacCode){
       response.label = "Activity Address Code (AAC):";
       response.value = data.aacCode;
     }
-    else if(typeof data.fpdsOrgId != 'undefined' || data.fpdsOrgId != null || data.fpdsOrgId != ''){
+    else if(data.fpdsOrgId){
       response.label = "FPDS Org ID:";
       response.value = data.fpdsOrgId;
     }
-    else if(typeof data.fpdsCode != 'undefined' || data.fpdsCode != null || data.fpdsCode != ''){
+    else if(data.fpdsCode){
       response.label = "FPDS Code:";
       response.value = data.fpdsCode;
     }
-    else if(typeof data.oldFPDSCode != 'undefined' || data.oldFPDSCode != null || data.oldFPDSCode != ''){
-     // don't have to set label since it is this label by default
+    else if(data.oldFPDSCode){
+      // don't have to set label since it is this label by default
       response.value = data.oldFPDSCode;
     }
 
