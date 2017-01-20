c0b1f0d43953c305a93d8bce481027b71b4f6f54
MatthewFinnell <MatthewFinnell@POWERBROKER04.CSP.MICROPAAS.IO>
2017-01-20 14:27:41 -0500

    minor update

--- src/app/entity/entity-object-poc.component.ts
+++ src/app/entity/entity-object-poc.component.ts
@@ -5,28 +5,22 @@ import { HostListener, Component, ElementRef, Input, Renderer, OnInit } from '@a
   template: `
 	<div *ngIf="poc.pocType">
 		<strong>{{poc.pocType}}</strong>
-		<br />
 	</div>
 	<div *ngIf="poc.pocFirstName || poc.pocMiddleName || poc.pocLastName">
 			<strong>Name: </strong>{{poc.pocFirstName}}<div *ngIf="poc.pocMiddleName" class="display-inline"> {{poc.pocMiddleName}}</div> {{poc.pocLastName}}
-		<br />
 	</div>
 	<div *ngIf="poc.pocTitle">
 		<strong>Title: </strong>{{poc.pocTitle}}
-		<br />
 	</div>
 	<div *ngIf="poc.pocUSPhone">
 		<strong>U.S. Phone: </strong>{{poc.pocUSPhone}}
 		<div class="display-inline" *ngIf="poc.pocUSPhoneExt"> ext. {{poc.pocUSPhoneExt}}</div>
-		<br />
 	</div>
 	<div *ngIf="poc.pocNonUSPhone">
 		<strong>Non U.S. Phone: </strong>{{poc.pocNonUSPhone}}
-		<br />
 	</div>
 	<div *ngIf="poc.pocFax">
 		<strong>Fax: </strong>{{poc.pocFax}}
-		<br />
 	</div>
 	<div *ngIf="poc.pocEmail">
 		<strong>Email: </strong>{{poc.pocEmail}}
