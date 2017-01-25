a1e7b2a826812ee160fafc23ea709906978cd13d
nathansun <nathan.sun@octoconsulting.com>
2017-01-25 10:47:49 -0500

    Fix zoom in for search header

--- src/ui-kit/search-header/search-header.component.ts
+++ src/ui-kit/search-header/search-header.component.ts
@@ -4,20 +4,28 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   selector: 'samSearchHeader',
   template: `
     <header id="sam-search-header" class='search-header'>
-      <div class="usa-grid align-top">
-        <div class="header-container">
-          <a class="logo-img" [routerLink]="['/']">
-            <img src="src/assets/img/transition-sam-logo.png" alt="Sam.gov Logo">
-          </a>
-          <samSearchbar [size]="'small'" (onSearch)="onSearchEvent($event)" 
-           [keyword]="keyword" [placeholder]="'#keyword'" [filterValue]="filterValue"></samSearchbar>          
-        </div>   
+      <div class="usa-width-one-whole">
+        <div class="usa-width-two-thirds align-top m_R-0">
+          <div class="header-container usa-width-one-whole">
+            <a class="logo-img m_R-0" [routerLink]="['/']">
+              <img src="src/assets/img/transition-sam-logo.png" alt="Sam.gov Logo">
+            </a>
+            <div class="search-bar-container">
+              <samSearchbar [size]="'small'" (onSearch)="onSearchEvent($event)" 
+             [keyword]="keyword" [placeholder]="'#keyword'" [filterValue]="filterValue"></samSearchbar>          
+            </div>
+          </div>   
+        </div>
+        <div class="usa-width-one-third"> 
+          <div class="pull-right">
+            <img title="An official website of the United States Government" class="image-wrap"
+               src="src/assets/img/us_flag_small.png" alt="US Flag Logo"/>
+          </div>
+          <SamHeaderLinks (onDropdownToggle)="dropdownEventControl($event)"></SamHeaderLinks>
+
+        </div>
       </div>
-      <div class="m_R-2x align-top pull-right">
-        <img title="An official website of the United States Government" class="image-wrap"
-           src="src/assets/img/us_flag_small.png" alt="US Flag Logo"/>
-      </div>
-      <SamHeaderLinks (onDropdownToggle)="dropdownEventControl($event)"></SamHeaderLinks>
+      
     </header>
 `
 })
