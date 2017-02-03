c88cc35b2c533a068647114776cafaea7a8f6ee8
Diego Ruiz <diego.ruiz@reisystems.com>
2017-02-03 11:38:10 -0500

    unit test fixes

--- src/app/app-components/display-page/display-page.spec.ts
+++ src/app/app-components/display-page/display-page.spec.ts
@@ -6,6 +6,8 @@ import { WrapperService, FHService } from 'api-kit';
 import { BaseRequestOptions, ConnectionBackend, Http } from '@angular/http';
 import { Observable } from 'rxjs';
 import { SamUIKitModule } from 'ui-kit';
+import { RouterTestingModule } from '@angular/router/testing';
+import {By} from '@angular/platform-browser';
 
 import { DisplayPageComponent } from './display-page.component';
 
@@ -29,7 +31,7 @@ describe('DisplayPageTests', () => {
         },
         //end,
       ],
-      imports: [FormsModule,SamUIKitModule]
+      imports: [FormsModule,SamUIKitModule,RouterTestingModule]
     });
     TestBed.compileComponents().then( ()=>{
       fixture = TestBed.createComponent(DisplayPageComponent);
@@ -40,11 +42,55 @@ describe('DisplayPageTests', () => {
 
   it('logo test', ()  => {
     let fakepath = "/this/is/a/fake/path.png";
-    comp.sidenavConfig = {};
+    //comp.sidenavConfig = {};
     comp.logoSrc = fakepath;
     fixture.detectChanges();
     fixture.whenStable().then(() => {
-      expect(comp.querySelector('.sidenav-logo').getAttribute("src")).toEqual(fakepath);
+      expect( fixture.debugElement.query( By.css('.sidenav-logo') ).nativeElement.getAttribute("src") ).toEqual(fakepath);
+    });
+	});
+  
+  it('sidenav test', ()  => {
+    comp.sidenavConfig = {
+      label: "test",
+      children: [
+        {
+          "label": "Award Details",
+          "route": "#opportunity-award",
+        },
+        {
+          "label": "General Information",
+          "route": "#opportunity-general",
+        },
+        {
+          "label": "Classification",
+          "route": "#opportunity-classification"
+        },
+        {
+          "label": "Synopsis/Description",
+          "route": "#opportunity-synopsis"
+        },
+        {
+          "label": "Packages",
+          "route": "#opportunity-packages"
+        },
+        {
+          "label": "Contact Information",
+          "route": "#opportunity-contact"
+        }
+      ]
+    };
+    
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      //console.log(fixture.debugElement.query( By.css('.usa-sidenav-list li:first-child a') ).nativeElement.innerHTML);
+      //console.log(comp.querySelector('samsidenav li:first-child a').innerHTML());
+      expect(fixture.debugElement.query( By.css('.usa-sidenav-list li:nth-child(1) a') ).nativeElement.innerHTML).toEqual("Award Details");
+      expect(fixture.debugElement.query( By.css('.usa-sidenav-list li:nth-child(2) a') ).nativeElement.innerHTML).toEqual("General Information");
+      expect(fixture.debugElement.query( By.css('.usa-sidenav-list li:nth-child(3) a') ).nativeElement.innerHTML).toEqual("Classification");
+      expect(fixture.debugElement.query( By.css('.usa-sidenav-list li:nth-child(4) a') ).nativeElement.innerHTML).toEqual("Synopsis/Description");
+      expect(fixture.debugElement.query( By.css('.usa-sidenav-list li:nth-child(5) a') ).nativeElement.innerHTML).toEqual("Packages");
+      expect(fixture.debugElement.query( By.css('.usa-sidenav-list li:nth-child(6) a') ).nativeElement.innerHTML).toEqual("Contact Information");
     });
 	});
 
