1c4fb8a1789507284a6dba3ded420678e693eded
Carlos Valle <carlos.m.valle@gmail.com>
2017-02-06 16:26:46 -0500

    Check if financial data has questions

--- src/app/assistance-listing/assistance-listing.chart.ts
+++ src/app/assistance-listing/assistance-listing.chart.ts
@@ -524,6 +524,7 @@ export class FinancialObligationChart {
   }
 
   prepareVisualizationData() {
+    console.log(this.financialData);
     let self = this;
     let formattedFinancialData = [];
     let obligations = d3.map();
@@ -565,7 +566,7 @@ export class FinancialObligationChart {
         let obligation = "No Obligation";
         if (item.assistanceType && item.assistanceType.length > 0) {
           obligation = getAssistanceType(item.assistanceType);
-        } else {
+        } else if(item.questions) {
           for (let question of item.questions) {
             if (question.questionCode === "salary_or_expense" && question.flag === "yes") {
               obligation = "Salary or Expense";
