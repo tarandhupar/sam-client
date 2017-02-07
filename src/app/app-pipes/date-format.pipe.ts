f6fa6e5c680fd4ce0d493fb370bbef58c66fff21
Kevin Song <kevin.song@reisystems.com>
2017-02-07 16:51:56 -0500

    Fix date bug

--- src/app/app-pipes/date-format.pipe.ts
+++ src/app/app-pipes/date-format.pipe.ts
@@ -4,7 +4,7 @@ import * as moment from 'moment/moment';
 @Pipe({name: 'dateFormat'})
 export class DateFormatPipe implements PipeTransform {
   transform(datetime:any, format:string) : any {
-    if (datetime === null) {
+    if (datetime == null) {
       return null;
     }
 
