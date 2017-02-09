d771b23ed35414b315e8669884897f3562d7887e
Michael Kellogg <michael.kellogg@C02RV0RHG8WL.local>
2017-02-09 08:43:22 -0500

    adding funding field

--- src/app/assistance-listing/search-result/assistance-listing-result.component.ts
+++ src/app/assistance-listing/search-result/assistance-listing-result.component.ts
@@ -28,6 +28,12 @@ import * as moment from 'moment/moment';
     	</div>
     	<div class="usa-width-one-third">
       	<ul class="usa-text-small m_B-0">
+        	<li><strong>Funded:</strong>
+          	<ul class="usa-unstyled-list">
+              <li *ngIf="data.isFunded">Yes</li>
+              <li *ngIf="!data.isFunded">No</li>
+            </ul>
+          </li>
         	<li><strong>CFDA Number</strong>
           	<ul class="usa-unstyled-list">
               <li class="fal-program-number">{{data.programNumber}}</li>
