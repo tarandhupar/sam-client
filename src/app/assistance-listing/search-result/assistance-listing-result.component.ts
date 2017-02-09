70f27ed804dd7c9fdf30cc7b9d9d847a5a6206f1
Michael Kellogg <michael.kellogg@C02RV0RHG8WL.local>
2017-02-09 10:04:08 -0500

    updating assistance listing search results template

--- src/app/assistance-listing/search-result/assistance-listing-result.component.ts
+++ src/app/assistance-listing/search-result/assistance-listing-result.component.ts
@@ -15,15 +15,22 @@ import * as moment from 'moment/moment';
       	<a *ngIf="data.isActive==true" [routerLink]="[printFALLink()]" [queryParams]="qParams">{{data.title}}</a>
       	<span *ngIf="data.isActive==false">{{data.title}}</span>
     	</h3>
+    	<ul class="usa-unstyled-list">    
+    	    <li>
+    	      <strong>CFDA Number: </strong><span class="fal-program-number">{{data.programNumber}}</span>
+          </li>
+      </ul>
+    	
+     
     	<div class="usa-width-two-thirds">
       	<p class="m_T-2x">
           {{data.objective | slice:0:150}}{{data.objective && data.objective.length > 150 ? ' ...' : ''}}
         </p>
       	<ul class="usa-unstyled-list usa-text-small m_T-3x m_B-2x">
         	<li *ngIf="data.organizationHierarchy && data.organizationHierarchy[0]?.level==1"><strong>Department:</strong><a href=""><span>{{data.organizationHierarchy[0].name}}</span></a></li>
-          <li *ngIf="data.organizationHierarchy && data.organizationHierarchy[1]?.level==2"><strong>Agency:</strong><a href=""><span>{{data.organizationHierarchy[1].name}}</span></a></li>
+          <!--<li *ngIf="data.organizationHierarchy && data.organizationHierarchy[1]?.level==2"><strong>Agency:</strong><a href=""><span>{{data.organizationHierarchy[1].name}}</span></a></li>-->
           <li *ngIf="data.organizationHierarchy && data.organizationHierarchy[2]?.level==3"><strong>Office:</strong><a href=""><span>{{data.organizationHierarchy[2].name}}</span></a></li>
-          <li *ngIf="data.contacts!=null"><strong>Headquarters Office:</strong> {{data.contacts[0].name}}<br>{{data.contacts[0].address?.streetAddress}}, {{data.contacts[0].address?.city}}, {{data.contacts[0].address?.state}} {{data.contacts[0].address?.zip}} {{data.contacts[0].address?.country}}</li>
+          <!--<li *ngIf="data.contacts!=null"><strong>Headquarters Office:</strong> {{data.contacts[0].name}}<br>{{data.contacts[0].address?.streetAddress}}, {{data.contacts[0].address?.city}}, {{data.contacts[0].address?.state}} {{data.contacts[0].address?.zip}} {{data.contacts[0].address?.country}}</li>-->
         </ul>
     	</div>
     	<div class="usa-width-one-third">
@@ -34,12 +41,7 @@ import * as moment from 'moment/moment';
               <li *ngIf="!data.isFunded">No</li>
             </ul>
           </li>
-        	<li><strong>CFDA Number</strong>
-          	<ul class="usa-unstyled-list">
-              <li class="fal-program-number">{{data.programNumber}}</li>
-            </ul>
-          </li>
-          <li><strong>Date Published</strong>
+          <li><strong>Last Date Modified</strong>
             <ul class="usa-unstyled-list">
               <li>{{data.publishDate}}</li>
             </ul>
