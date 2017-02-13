b22c025a1bc0961c92484f7220060115a3a151ac
Anurag Mundada <anurag.mundada@C02RV0QXG8WL.local>
2017-02-13 15:39:38 -0500

    Historical FAL records initial draft for history section

--- src/app/assistance-listing/search-result/assistance-listing-result.component.ts
+++ src/app/assistance-listing/search-result/assistance-listing-result.component.ts
@@ -9,7 +9,7 @@ import * as moment from 'moment/moment';
   template: `
       <p>
     	  <span class="usa-label">Federal Assistance Listing</span>
-    	  <span *ngIf="data.isActive==false" class="usa-label">ARCHIVED</span>
+    	  <span *ngIf="data.isActive==false" class="usa-label">HISTORICAL</span>
     	</p>
     	<h3 class="assistance-listing-title">
       	<a *ngIf="data.isActive==true" [routerLink]="['/programs', data._id, 'view']" [queryParams]="qParams">{{data.title}}</a>
@@ -20,25 +20,31 @@ import * as moment from 'moment/moment';
           {{data.objective | slice:0:150}}{{data.objective && data.objective.length > 150 ? ' ...' : ''}}
         </p>
       	<ul class="usa-unstyled-list usa-text-small m_T-3x m_B-2x">
-        	<li *ngIf="data.organizationHierarchy && data.organizationHierarchy[0]?.level==1"><strong>Department:</strong><a href=""><span>{{data.organizationHierarchy[0].name}}</span></a></li>
-          <li *ngIf="data.organizationHierarchy && data.organizationHierarchy[1]?.level==2"><strong>Agency:</strong><a href=""><span>{{data.organizationHierarchy[1].name}}</span></a></li>
+        	<li *ngIf="data.organizationHierarchy && data.organizationHierarchy[0]?.level==1"><strong>Department/Ind. Agency:</strong><a href=""><span>{{data.organizationHierarchy[0].name}}</span></a></li>
           <li *ngIf="data.organizationHierarchy && data.organizationHierarchy[2]?.level==3"><strong>Office:</strong><a href=""><span>{{data.organizationHierarchy[2].name}}</span></a></li>
-          <li *ngIf="data.contacts!=null"><strong>Headquarters Office:</strong> {{data.contacts[0].name}}<br>{{data.contacts[0].address?.streetAddress}}, {{data.contacts[0].address?.city}}, {{data.contacts[0].address?.state}} {{data.contacts[0].address?.zip}} {{data.contacts[0].address?.country}}</li>
         </ul>
+        <!--History section to be displayed only for historical records-->
+        <div *ngIf="data.isActive==false">
+        <strong class=usa-external_link (click)="toggleHistory()">History</strong>
+        <div *ngIf="toggleField">
+        <ul>
+          <li *ngFor="let item of history" class="current">
+            <span>  
+            <strong>{{item.createdDate | date:"yyyy"}}</strong>: <span>{{item.body}}</span>
+            </span>
+          </li>
+        </ul>
+        </div>
+        </div>
     	</div>
     	<div class="usa-width-one-third">
       	<ul class="usa-text-small m_B-0">
-        	<li><strong>CFDA Number</strong>
-          	<ul class="usa-unstyled-list">
-              <li class="fal-program-number">{{data.programNumber}}</li>
-            </ul>
-          </li>
           <li><strong>Date Published</strong>
             <ul class="usa-unstyled-list">
               <li>{{data.publishDate}}</li>
             </ul>
           </li>
-          <li><strong>Type</strong>
+          <li *ngIf="data.isActive==true"><strong>Type</strong>
             <ul class="usa-unstyled-list">
               <li><span *ngFor="let assistanceTypes of data.assistanceTypes; let i=index">{{ assistanceTypes.code }} {{ assistanceTypes.code!==null ? '-' : '' }} {{ assistanceTypes.value }}{{ assistanceTypes.value!==null && i!==data.assistanceTypes.length-1 ? ',' : '' }}</span></li>
             </ul>
@@ -50,10 +56,17 @@ import * as moment from 'moment/moment';
 export class AssistanceListingResult implements OnInit {
 	@Input() data: any={};
   @Input() qParams:any = {};
+  history: any = [];
+  toggleField: boolean;
 	constructor() { }
 
   ngOnInit(){
     this.data.publishDate = moment(this.data.publishDate).format("MMM D, Y");
+    this.history = this.data.historicalIndex;
   }
-  
+
+  toggleHistory() {
+    this.toggleField = !this.toggleField;
+  }
+
 }
