a883d4f33c7938991486c60c3c24ad87d3780625
dmitriy.ivanovskiy <dmitriy.ivanovskiy@reisystems.com>
2017-02-24 10:57:10 -0500

    SAM-6222 fixed the bug and added a pipe

---
+++ src/app/wage-determination/pipes/states-counties.spec.ts
@@ -0,0 +1,17 @@
+import { StatesCountiesPipe } from "./states-counties.pipe";
+
+fdescribe('StatesCountiesPipe', () => {
+  let pipe = new StatesCountiesPipe();
+  let dictionaries = {county: [{dictionary_name:"county",element_id:"14343",value:"Navajo"}],
+    state: [{dictionary_name:"state",element_id:"AL",value:"Alabama"}]};
+  let location = [
+    {
+      "state": "AL",
+      "statewideFlag": false,
+      "counties": [14343]
+    }];
+  it('transforms an object to a string', () => {
+    expect(pipe.transform(location, dictionaries)).toEqual({ states: 'Alabama', counties: 'Alabama - Navajo' });
+  });
+
+});
