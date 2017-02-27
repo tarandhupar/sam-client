88c28479f17774b74d75168808f954bdf1751b25
Diego Ruiz <diego.ruiz@reisystems.com>
2017-02-27 14:50:48 -0500

    unit test fixes

--- src/app/app-components/display-page/display-page.spec.ts
+++ src/app/app-components/display-page/display-page.spec.ts
@@ -7,7 +7,7 @@ import { DisplayPageComponent } from './display-page.component';
 
 var fixture;
 var comp;
-var titleEl;
+let fakepath;
 
 describe('DisplayPageTests', () => {
   beforeEach(async(() => {
@@ -18,52 +18,49 @@ describe('DisplayPageTests', () => {
     TestBed.compileComponents().then( ()=>{
       fixture = TestBed.createComponent(DisplayPageComponent);
       comp = fixture.componentInstance;
+      fakepath = "/this/is/a/fake/path.png";
+      comp.logoData = {logo: fakepath, info: ""};
+      comp.sidenavConfig = {
+        label: "test",
+        children: [
+          {
+            "label": "Award Details",
+            "route": "#opportunity-award",
+          },
+          {
+            "label": "General Information",
+            "route": "#opportunity-general",
+          },
+          {
+            "label": "Classification",
+            "route": "#opportunity-classification"
+          },
+          {
+            "label": "Synopsis/Description",
+            "route": "#opportunity-synopsis"
+          },
+          {
+            "label": "Packages",
+            "route": "#opportunity-packages"
+          },
+          {
+            "label": "Contact Information",
+            "route": "#opportunity-contact"
+          }
+        ]
+      };
+      fixture.detectChanges();
     });
 
   }));
 
   it('logo test', ()  => {
-    let fakepath = "/this/is/a/fake/path.png";
-    //comp.sidenavConfig = {};
-    comp.logoData = {logo: fakepath, info: ""};
-    fixture.detectChanges();
     fixture.whenStable().then(() => {
       expect( fixture.debugElement.query( By.css('.sidenav-logo') ).nativeElement.getAttribute("src") ).toEqual(fakepath);
     });
 	});
 
   it('sidenav test', ()  => {
-    comp.sidenavConfig = {
-      label: "test",
-      children: [
-        {
-          "label": "Award Details",
-          "route": "#opportunity-award",
-        },
-        {
-          "label": "General Information",
-          "route": "#opportunity-general",
-        },
-        {
-          "label": "Classification",
-          "route": "#opportunity-classification"
-        },
-        {
-          "label": "Synopsis/Description",
-          "route": "#opportunity-synopsis"
-        },
-        {
-          "label": "Packages",
-          "route": "#opportunity-packages"
-        },
-        {
-          "label": "Contact Information",
-          "route": "#opportunity-contact"
-        }
-      ]
-    };
-
-    fixture.detectChanges();
     fixture.whenStable().then(() => {
       expect(fixture.debugElement.query( By.css('.usa-sidenav-list li:nth-child(1) a') ).nativeElement.innerHTML).toEqual("Award Details");
       expect(fixture.debugElement.query( By.css('.usa-sidenav-list li:nth-child(2) a') ).nativeElement.innerHTML).toEqual("General Information");
