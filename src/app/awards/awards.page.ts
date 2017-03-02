4f7db0b3bccec9b783d4fa8842efe42cc72a081a
MatthewFinnell <MatthewFinnell@POWERBROKER04.CSP.MICROPAAS.IO>
2017-03-02 16:01:45 -0500

    functions to call OT APIs

--- src/app/awards/awards.page.ts
+++ src/app/awards/awards.page.ts
@@ -28,13 +28,13 @@ export class AwardsPage implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.currentUrl = this.location.path();
-    this.loadAwardData();
+    this.loadOTIData();
   }
   
   private loadAwardData() {
     let apiSubject = new ReplaySubject(1); // broadcasts the api data to multiple subscribers
     let apiStream = this.activatedRoute.params.switchMap(params => { // construct a stream of api data
-      return this.AwardsService.getAwardsData("7529", "HHSN30700010", "0", "0", "A");
+      return this.AwardsService.getAwardsData("4740", "GS04P14EXC0125", "PS03", "0", "D");
     });
     apiStream.subscribe(apiSubject);
 
@@ -49,7 +49,37 @@ export class AwardsPage implements OnInit, OnDestroy {
    private loadIDVData() {
     let apiSubject = new ReplaySubject(1); // broadcasts the api data to multiple subscribers
     let apiStream = this.activatedRoute.params.switchMap(params => { // construct a stream of api data
-      return this.AwardsService.getIDVData("6930", "DTFR5309A00013", "2", "E");
+      return this.AwardsService.getIDVData("3600", "VA24615A0024", "0", "E");
+    });
+    apiStream.subscribe(apiSubject);
+
+    this.subscription = apiSubject.subscribe(api => { // run whenever api data is updated
+      let jsonData:any = api;
+      this.awardData = jsonData;
+    }, err => {
+      console.log('Error logging', err);
+    });
+   }
+   
+   private loadOTAData() {
+    let apiSubject = new ReplaySubject(1); // broadcasts the api data to multiple subscribers
+    let apiStream = this.activatedRoute.params.switchMap(params => { // construct a stream of api data
+      return this.AwardsService.getOTAData("7013", "HSTS0213HSLR316", "P00003", "0", "R");
+    });
+    apiStream.subscribe(apiSubject);
+
+    this.subscription = apiSubject.subscribe(api => { // run whenever api data is updated
+      let jsonData:any = api;
+      this.awardData = jsonData;
+    }, err => {
+      console.log('Error logging', err);
+    });
+   }
+   
+    private loadOTIData() {
+    let apiSubject = new ReplaySubject(1); // broadcasts the api data to multiple subscribers
+    let apiStream = this.activatedRoute.params.switchMap(params => { // construct a stream of api data
+      return this.AwardsService.getOTIData("9700", "HR00111590002", "P00002", "O");
     });
     apiStream.subscribe(apiSubject);
 
