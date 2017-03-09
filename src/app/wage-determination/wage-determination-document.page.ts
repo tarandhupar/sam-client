f00c29520f80f1478764eeaef5faf0a541076e08
Carlos Valle <carlos.m.valle@gmail.com>
2017-03-09 14:14:30 -0500

    Theming

--- src/app/wage-determination/wage-determination-document.page.ts
+++ src/app/wage-determination/wage-determination-document.page.ts
@@ -1,11 +1,33 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
 import { WageDeterminationService } from "api-kit";
 
 
 @Component({
   moduleId: __filename,
-  template: `<pre *ngIf="wageDetermination">{{wageDetermination.document}}</pre>`,
+  template: `
+    <div class="usa-grid">
+      <div class="usa-width-whole">
+        <pre *ngIf="wageDetermination">
+          {{wageDetermination.document}}
+        </pre>
+      </div>
+    </div>
+  `,
+  styles:[`
+     @media print{
+       body *{
+         visibility: hidden;
+       }
+       samsearchheader, samfooter{
+         display: none;
+       }
+       body main pre{
+         visibility: visible
+       }
+     }
+  `],
+  encapsulation: ViewEncapsulation.None,
   providers: [
     WageDeterminationService
   ]
