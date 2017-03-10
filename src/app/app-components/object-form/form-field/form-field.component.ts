d851993a535c074ab207bef9daea7ddd5cb34943
ydave-reisys <ydave@reisys.com>
2017-03-10 16:43:01 -0500

    common structure for object Create Update forms

--- src/app/app-components/object-form/form-field/form-field.component.ts
+++ src/app/app-components/object-form/form-field/form-field.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, forwardRef,  OnInit } from '@angular/core';
-import {NG_VALUE_ACCESSOR, ControlValueAccessor} from "@angular/forms";
-
+import {NG_VALUE_ACCESSOR, ControlValueAccessor, FormGroup} from "@angular/forms";
 
 export const FORM_FIELD_VALUE_ACCESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
@@ -13,35 +12,30 @@ export const FORM_FIELD_VALUE_ACCESSOR: any = {
   templateUrl: 'form-field.template.html',
   providers: [ FORM_FIELD_VALUE_ACCESSOR ]
 })
-export class FormFieldComponent implements ControlValueAccessor, OnInit{
+export class FormFieldComponent implements ControlValueAccessor{
   @Input() field;
   @Input() value;
   @Input() control;
-
-  private _formControlName;
+  @Input() fieldFormGroup: FormGroup;
 
   onChange: any = () => { };
   onTouched: any = () => { };
 
-  ngOnInit(){
-      this.writeValue("Test23");
+  /*ngOnInit(){
+     this.writeValue("Test23");
       this.control.patchValue("kajdsaljd");
       this.registerOnTouched(() => { });
-  }
+  }*/
 
   registerOnChange(fn) {
     this.onChange = fn;
-    console.log("registeronchange");
   }
 
   registerOnTouched(fn) {
     this.onTouched = fn;
-    console.log("registerontouch");
   }
 
   writeValue(value) {
-    //this._formControlName = value;
-    //console.log("writevalue", value);
     this.value = value;
   }
 
--- src/app/app-components/object-form/object-form.component.ts
+++ src/app/app-components/object-form/object-form.component.ts
@@ -12,9 +12,10 @@ import { Component, Input } from '@angular/core';
 export class ObjectFormModel{
   public mainForm: FormGroup;
   @Input() public objectFormData;
+  @Input() public objectService;
+  @Input() public serviceMethod;
 
   constructor(private fb: FormBuilder) {}
-
 
   createForm(objectFormData){
     this.objectFormData = objectFormData;
@@ -24,16 +25,49 @@ export class ObjectFormModel{
       // dynamically generate the control groups
       let formGroup = {};
       for (let field of section.fields) {
-        //console.log(field.name);
-        //console.log(field.attributes.control);
         formGroup[field.name] = '';
       }
 
       sections[section.section] = this.fb.group(formGroup);
-      //console.log(section);
     }
     this.mainForm = this.fb.group(sections);
 
-    //return this.mainForm;
   }
+
+
+  onCancelClick(event) {
+    console.log("cancel clicked");
+  }
+
+  onSaveContinueClick(event){
+    console.log("save and continue clicked");
+    let data = {
+      "title": "Test123",
+      "alternativeNames": ['Test'],
+      "programNumber": '124'
+    };
+
+     this.objectService.getProgramById('a3ad810cad0f5371dadbd820eb8ed8d3')
+      .subscribe(id => {
+        console.log('AJAX Completed', id);
+        /*if(this.programId == null) {
+         this.programForm.reset();
+         this.successMsg = "New Assistance Listing is successfully added.";
+         }
+         else
+         this.successMsg = "Assistance Listing is successfully updated.";
+
+         this.submitted = true;*/
+
+      });
+  }
+
+  onSaveExitClick(event){
+    console.log("save and exit clicked");
+  }
+
+  onPreviousClick(event){
+    console.log("previous clicked");
+  }
+
 }
