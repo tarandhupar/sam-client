809ff2dd4cc5fb875e38032b02d2090380d70544
Justin Babbs <justin.babbs@octoconsulting.com>
2017-03-13 14:04:58 -0400

    add a header to grant access, flip role and domain

---
+++ src/api-kit/access/domain.interface.ts
@@ -0,0 +1,36 @@
+export interface IDomain {
+  "_embedded": {
+    "domainList": [
+      {
+        "_links": {
+          "self": {
+            "href": "https://csp-api.sam.gov:443/rms/v1/domains/1"
+          }
+        },
+        "pk_domain": 1,
+        "domainName": "AWARD",
+        "legacyDomains": "FPDS,",
+        "is_active": true,
+        "created_by": "RMS",
+        "created_date": "03/09/2017",
+        "updated_by": "RMS",
+        "updated_date": "03/09/2017"
+      },
+      {
+        "_links": {
+          "self": {
+            "href": "https://csp-api.sam.gov:443/rms/v1/domains/2"
+          }
+        },
+        "pk_domain": 2,
+        "domainName": "OPPORTUNITY",
+        "legacyDomains": "FBO,",
+        "is_active": true,
+        "created_by": "RMS",
+        "created_date": "03/09/2017",
+        "updated_by": "RMS",
+        "updated_date": "03/09/2017"
+      }
+    ]
+  }
+}
