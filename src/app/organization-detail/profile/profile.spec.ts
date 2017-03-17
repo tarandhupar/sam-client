4e8e8032217c4d9ebf6d130526675c0129c31bda
nathansun <nathan.sun@octoconsulting.com>
2017-03-16 20:41:47 -0400

    redeploy

--- src/app/organization-detail/profile/profile.spec.ts
+++ src/app/organization-detail/profile/profile.spec.ts
@@ -124,7 +124,7 @@ let activatedRouteStub = {
   }
 };
 
-fdescribe('Organization Detail Profile Page', () => {
+describe('Organization Detail Profile Page', () => {
   // provide our implementations or mocks to the dependency injector
   let component:OrgDetailProfilePage;
   let fixture:any;
