1c7c755f5082cb2b63a99a89fdfa90dc1539a364
Kevin Chiu <chiu_kevin@bah.com>
2017-03-27 19:20:31 -0400

    - Added view component for system account password managememt - Restructured  subroute system for system account pages

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -17,7 +17,11 @@ import { SamKBAComponent, SamPasswordComponent } from './shared';
 import { LoginComponent } from './login';
 import { RegisterComponent, RegisterInitialComponent, RegisterConfirmComponent, RegisterMainComponent } from './register';
 import { ForgotComponent, ForgotInitialComponent, ForgotConfirmComponent, ForgotMainComponent } from './forgot';
-import { ProfileComponent, DetailsComponent, SystemDetailsComponent, ResetComponent, MigrationsComponent } from './profile';
+import {
+  ProfileComponent,
+  DetailsComponent, ResetComponent, MigrationsComponent,
+  SystemDetailsComponent, SystemResetComponent
+} from './profile';
 
 @NgModule({
   imports: [
@@ -62,10 +66,13 @@ import { ProfileComponent, DetailsComponent, SystemDetailsComponent, ResetCompon
      * Profile
      */
     ProfileComponent,
+    // User
     DetailsComponent,
     MigrationsComponent,
     ResetComponent,
+    // System
     SystemDetailsComponent,
+    SystemResetComponent
   ],
 
   providers: [
