2bf8ffded22bd27bac23a58b91f81c4a96bebff1
Anurag Mundada <anurag.mundada@C02RV0QXG8WL.local>
2017-03-27 13:23:44 -0400

    bug fix

--- src/app/awards/search-result/awards-result.component.ts
+++ src/app/awards/search-result/awards-result.component.ts
@@ -34,7 +34,7 @@ import Moment = moment.Moment;
               <li><span>{{ data.contract?.obligatedAmount | currency:'USD':true }}</span></li>
             </ul>
           </li>    
-          <li><strong>Award Type </strong>
+          <li><strong>{{ data.type=='AWARD' ? 'Award Type' : 'IDV Type' }} </strong>
             <ul class="usa-unstyled-list">
               <li><span>{{ data.awardType?.value }}</span></li>
             </ul>
