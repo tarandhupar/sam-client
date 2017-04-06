d481c1dc31a706942bdf5b77f9ef439a9d21766d
dmitriy.ivanovskiy <dmitriy.ivanovskiy@reisystems.com>
2017-04-06 14:05:02 -0400

    SAM-8733

--- src/app/app-pipes/date-format.pipe.ts
+++ src/app/app-pipes/date-format.pipe.ts
@@ -1,5 +1,6 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import * as moment from 'moment/moment';
+import 'moment-timezone';
 
 @Pipe({name: 'dateFormat'})
 export class DateFormatPipe implements PipeTransform {
@@ -8,6 +9,6 @@ export class DateFormatPipe implements PipeTransform {
       return null;
     }
 
-    return moment(datetime).format(format);
+    return moment(datetime).tz(moment.tz.guess()).format(format)
   }
 }
