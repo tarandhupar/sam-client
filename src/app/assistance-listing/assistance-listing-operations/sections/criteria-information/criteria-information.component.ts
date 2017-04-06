beb061af66ba7216089955e906b47b74208a8f28
Madhavi Ambati <mambati@techflow.com>
2017-04-06 10:50:33 -0400

    SAM-6824

--- src/app/assistance-listing/assistance-listing-operations/sections/criteria-information/criteria-information.component.ts
+++ src/app/assistance-listing/assistance-listing-operations/sections/criteria-information/criteria-information.component.ts
@@ -1,7 +1,6 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
-import {FormBuilder, FormArray, FormGroup, Validators} from '@angular/forms';
+import {Component, OnInit, OnDestroy, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
-import {UUID} from 'angular2-uuid';
 import {ProgramService} from 'api-kit';
 import {FALOpSharedService} from '../../assistance-listing-operations.service';
 import {DictionaryService} from 'api-kit';
@@ -16,10 +15,27 @@ export class FALCriteriaInfoComponent implements OnInit, OnDestroy {
   falCriteriaForm: FormGroup;
   redirectToViewPg: boolean;
   redirectToWksp: boolean;
+  saveProgSub: any;
+  getProgSub: any;
+  assUasageDictSub: any;
+  programTitle: string;
+  assUsageOptions = [];
+  assUsageTypeArray = [];
+  toggleBeneficiarySection: boolean = false;
+  awardedTextarea: boolean = false;
+
+
   applicantTypes = ['applicant1', 'applicant2', 'applicant3'];
   assiUsageTypes = ['assiatnceuasage1', 'assiatnceuasage2', 'assiatnceuasage3'];
   benTypes = ['ben1', 'ben2', 'ben3'];
-  awardedTypesoptions = ['award1', 'award2', 'award3'];
+  awardedTypesOptions = [{
+    code: 'other',
+    name: 'other'
+  }];
+
+
+  @ViewChild('awardedTypeWrapper') awardedTypeWrapper;
+  @ViewChild('assiUsageTypeWrapper') assiUsageTypeWrapper;
 
   // Checkboxes Component
   benSameAsApplicantConfig = {
@@ -27,7 +43,7 @@ export class FALCriteriaInfoComponent implements OnInit, OnDestroy {
       {
         value: 'isSameAsApplicant',
         label: 'Beneficiary eligibility is the same as applicant eligibility',
-        name: 'checkbox-sa'
+        name: 'checkbox-isa'
       },
     ],
   };
@@ -48,26 +64,41 @@ export class FALCriteriaInfoComponent implements OnInit, OnDestroy {
               private programService: ProgramService,
               private router: Router,
               private sharedService: FALOpSharedService,
-              private dictService: DictionaryService) {
+              private dictionaryService: DictionaryService) {
     sharedService.setSideNavFocus();
     this.programId = sharedService.programId;
+    this.assUasageDictSub = dictionaryService.getDictionaryById('assistance_type')
+      .subscribe(data => {
+        for (let dictionaries of data['assistance_type']) {
+          for (let element of dictionaries.elements) {
+            let elementId = element.element_id;
+            let value = element.value;
+            let code = element.code;
+            this.assUsageOptions.push({
+              code: elementId,
+              name: code + ' - ' + value
+            });
+            this.assUsageTypeArray[elementId] = name;
+          }
+        }
+      });
   }
 
   ngOnInit() {
     this.createForm();
-    /* if (this.sharedService.programId) {
-     this.getData();
-     }*/
+    if (this.sharedService.programId) {
+      this.getData();
+    }
   }
 
   ngOnDestroy() {
-    /*    if (this.saveProgSub)
-     this.saveProgSub.unsubscribe();
+    if (this.saveProgSub)
+      this.saveProgSub.unsubscribe();
 
-     if (this.getProgSub)
-     this.getProgSub.unsubscribe();
+    if (this.getProgSub)
+      this.getProgSub.unsubscribe();
 
-     if (this.dictSubState)
+    /*if (this.dictS)
      this.dictSubState.unsubscribe();*/
 
   }
@@ -75,18 +106,82 @@ export class FALCriteriaInfoComponent implements OnInit, OnDestroy {
   createForm() {
     this.falCriteriaForm = this.fb.group({
       'documentation': '',
-      'applicantTypes': '',
+      // 'applicantTypes': '',
       'applicantDesc': '',
-      'benSameAsApplicant': '',
-      'benTypes': '',
-      'benDescription': '',
+      'isSameAsApplicant': '',
+      //'benTypes': '',
+      'benDesc': '',
       'lengthTimeDesc': '',
       'awardedTypes': '',
-      'awardedDesc':'',
-      'assiUsageTypes': '',
-      'assiUsageDesc': '',
-      'usageResDesc':''
+      'awardedDesc': '',
+      //'assUsageTypes': '',
+      'assUsageDesc': '',
+      'usageResDesc': ''
     });
+  }
+
+  getData() {
+    this.getProgSub = this.programService.getProgramById(this.sharedService.programId, this.sharedService.cookieValue)
+      .subscribe(api => {
+          this.programTitle = api.data.title;
+          let documentation = '';
+          let applicantDesc = '';
+          let isSameAsApplicant = '';
+          let benDesc = '';
+          let lengthTimeDesc = '';
+          let awardedDesc = '';
+          let assUsageDesc = '';
+          let usageResDesc = '';
+          if (api.data) {
+            if (api.data.eligibility) {
+              if (api.data.eligibility.beneficiary) {
+                if (api.data.eligibility.beneficiary.isSameAsApplicant) {
+                  isSameAsApplicant = (api.data.eligibility.beneficiary.isSameAsApplicant ? api.data.eligibility.beneficiary.isSameAsApplicant : '');
+                }
+                benDesc = api.data.eligibility.beneficiary.description ? api.data.eligibility.beneficiary.description : '';
+              }
+              documentation = api.data.eligibility.documentation.description ? api.data.eligibility.documentation.description : '';
+              applicantDesc = api.data.eligibility.applicant.description ? api.data.eligibility.applicant.description : '';
+              lengthTimeDesc = api.data.eligibility.limitation.description ? api.data.eligibility.limitation.description : '';
+              awardedDesc = api.data.eligibility.limitation.awardedDescription ? api.data.eligibility.limitation.awardedDescription : '';
+              assUsageDesc = api.data.eligibility.assistanceUsage.description ? api.data.eligibility.assistanceUsage.description : '';
+              usageResDesc = api.data.eligibility.usage.restrictions.description ? api.data.eligibility.usage.restrictions.description : '';
+            }
+          }
+
+          if (isSameAsApplicant) {
+            isSameAsApplicant = 'isSameAsApplicant';
+            this.toggleBeneficiarySection = false;
+            this.falCriteriaForm.value.benDesc = '';
+          } else {
+            this.toggleBeneficiarySection = true;
+          }
+          this.falCriteriaForm.patchValue({
+            documentation: documentation,
+            applicantDesc: applicantDesc,
+            isSameAsApplicant: [isSameAsApplicant],
+            benDesc: benDesc,
+            lengthTimeDesc: lengthTimeDesc,
+            awardedDesc: awardedDesc,
+            assUsageDesc: assUsageDesc,
+            usageResDesc: usageResDesc
+          });
+        },
+        error => {
+          console.error('Error Retrieving Program!!', error);
+        });//end of subscribe
+
+  }
+
+  awardedTypeChange(event) {
+    this.awardedTypeWrapper.formatErrors(this.falCriteriaForm.controls['awardedTypes']);
+    console.log('this.falCriteriaForm.value.awardedTypes........',this.falCriteriaForm.value.awardedTypes)
+    if(this.falCriteriaForm.value.awardedTypes==='other')
+    this.awardedTextarea = true;
+  }
+
+  assiUsageTypeChange(event) {
+    this.assiUsageTypeWrapper.formatErrors(this.falCriteriaForm.controls['assiUsageTypes'])
   }
 
   onCancelClick(event) {
@@ -116,7 +211,94 @@ export class FALCriteriaInfoComponent implements OnInit, OnDestroy {
     this.saveData();
   }
 
+  chkSameAsApp(event) {
+    if (event.indexOf('isSameAsApplicant') !== -1) {
+      this.toggleBeneficiarySection = false;
+      this.falCriteriaForm.value.benDesc = '';
+    } else {
+      this.toggleBeneficiarySection = true;
+    }
+
+  }
+
   saveData() {
-    console.log('save data', this.falCriteriaForm.value);
+    let criteriaData = {};
+    let data = {};
+    let applicantTypes = this.falCriteriaForm.value.applicantTypes;
+    let applicantDesc = this.falCriteriaForm.value.applicantDesc;
+    let assUsageTypes = this.falCriteriaForm.value.assUsageTypes;
+    let assUsageDesc = this.falCriteriaForm.value.assUsageDesc;
+    let isSameAsApplicant: boolean;
+    if (this.falCriteriaForm.value.isSameAsApplicant) {
+      isSameAsApplicant = this.falCriteriaForm.value.isSameAsApplicant.indexOf('isSameAsApplicant') !== -1;
+    }
+    let benTypes = this.falCriteriaForm.value.benTypes;
+    let benDesc = this.falCriteriaForm.value.benDesc;
+    let documentation = this.falCriteriaForm.value.documentation;
+    let lengthTimeDesc = this.falCriteriaForm.value.lengthTimeDesc;
+    let awardedTypes = this.falCriteriaForm.value.awardedTypes;
+    let awardedDesc = this.falCriteriaForm.value.awardedDesc;
+    let usageResDesc = this.falCriteriaForm.value.usageResDesc;
+
+    criteriaData = this.buildJson(applicantTypes, applicantDesc, assUsageTypes, assUsageDesc, isSameAsApplicant, benTypes, benDesc,
+      documentation, lengthTimeDesc, awardedTypes, awardedDesc, usageResDesc);
+
+    data = {
+      "eligibility": criteriaData
+    }
+    console.log('save data', data);
+    this.saveProgSub = this.programService.saveProgram(this.sharedService.programId, data, this.sharedService.cookieValue)
+      .subscribe(api => {
+          this.sharedService.programId = api._body;
+          console.log('AJAX Completed Criteria Information', api);
+
+          if (this.redirectToWksp)
+            this.router.navigate(['falworkspace']);
+          else if (this.redirectToViewPg) {
+            this.router.navigate(['/programs', this.sharedService.programId, 'view']);
+          }
+          else
+            this.router.navigate(['/programs/' + this.sharedService.programId + '/edit/compliance-requirements']);
+
+        },
+        error => {
+          console.error('Error saving Program - Criteria Information Section!!', error);
+        });
+  }
+
+  buildJson(applicantTypes: any, applicantDesc: string, assUsageTypes: any, assUsageDesc: string,
+            isSameAsApplicant: boolean, benTypes: any, benDesc: string, documentation: string,
+            lengthTimeDesc: string, awardedTypes: any, awardedDesc: string, usageResDesc: string): any {
+
+    let data = {
+      "applicant": {
+        "types": applicantTypes,
+        "description": applicantDesc
+      },
+      "assistanceUsage": {
+        "types": assUsageTypes,
+        "description": assUsageDesc
+      },
+      "beneficiary": {
+        "isSameAsApplicant": isSameAsApplicant,
+        "types": isSameAsApplicant ? '' : benTypes,
+        "description": isSameAsApplicant ? '' : benDesc
+      },
+      "documentation": {
+        "description": documentation
+      },
+      "limitation": {
+        // 102 (Length and Time Phasing)
+        "description": lengthTimeDesc,
+        "awarded": awardedTypes,
+        "awardedDescription": awardedDesc
+      },
+      "usage": {
+        "restrictions": {
+          "description": usageResDesc
+        }
+      }
+    };
+    return data;
   }
 }
