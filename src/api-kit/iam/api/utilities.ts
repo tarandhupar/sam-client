ea323fe1af7699d0a19a5b7b895e2ae16eff5d88
ydave-reisys <ydave@reisys.com>
2017-04-19 13:25:47 -0400

    revert bsp csp

--- src/api-kit/iam/api/utilities.ts
+++ src/api-kit/iam/api/utilities.ts
@@ -3,9 +3,9 @@ import config from './config';
 
 function $params() {
   let pattern = /([^&=]+)=?([^&]*)/g,
-      decodePattern = /\+/g,
-      decode,
-      parse;
+    decodePattern = /\+/g,
+    decode,
+    parse;
 
   decode = function(strValue) {
     return decodeURIComponent(
@@ -15,13 +15,13 @@ function $params() {
 
   parse = function(query) {
     let params = {},
-        output,
-        key,
-        val;
+      output,
+      key,
+      val;
 
     while (output = pattern.exec(query) ) {
-     key = decode(output[1])
-     val = decode(output[2]);
+      key = decode(output[1])
+      val = decode(output[2]);
 
       if(key.substring(key.length - 2) === '[]') {
         key = key.substring(0, key.length - 2);
@@ -39,7 +39,7 @@ function $params() {
 
 function $matcher(payload, pattern) {
   let matches = [],
-      output;
+    output;
 
   while(output = pattern.exec(payload)) {
     if(indexOf(matches, output[1]) == -1) {
@@ -52,10 +52,10 @@ function $matcher(payload, pattern) {
 
 function $sprintf(payload, data) {
   let pattern = new RegExp('\{([a-z0-9\-]+)\}', 'g'),
-      replace,
-      matches = $matcher(payload, pattern),
-      match,
-      intMatch;
+    replace,
+    matches = $matcher(payload, pattern),
+    match,
+    intMatch;
 
   if(isObject(data)) {
     for(intMatch = 0; intMatch < matches.length; intMatch++) {
@@ -72,10 +72,10 @@ function $sprintf(payload, data) {
 
 function parseUrlQuery() {
   let params = {},
-      query = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, (m, key, value) => {
-        params[key] = value.toString().match(/true|false/) ? (/^true$/i).test(value) : value;
-        return undefined;
-      });
+    query = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, (m, key, value) => {
+      params[key] = value.toString().match(/true|false/) ? (/^true$/i).test(value) : value;
+      return undefined;
+    });
 
   return params;
 }
@@ -130,11 +130,11 @@ class Utilities {
 
   getEnvironment(environments?) {
     let environment,
-    		hostname = location.host,
-    		active = 'local',
-    		patterns,
-    		pattern,
-    		intPattern;
+      hostname = location.host,
+      active = 'local',
+      patterns,
+      pattern,
+      intPattern;
 
     environments = environments || this.environments || {};
 
@@ -185,8 +185,8 @@ class Utilities {
 
   getUrl(endpoint, params?) {
     let $environment = (this.environment == 'local') ? 'comp' : this.environment,
-        routeParams = merge({}, (params || {}), { environment: $environment }),
-        url = $sprintf(endpoint, routeParams);
+      routeParams = merge({}, (params || {}), { environment: $environment }),
+      url = $sprintf(endpoint, routeParams);
 
     if(this.isRelative(url)) {
       url = [this.baseUri, url]
@@ -195,8 +195,8 @@ class Utilities {
     }
 
     // Apply API Key
-    if(url.search(/^https?:\/\/api-umbrella\.prod-iae\.bsp\.gsa\.gov/) > -1) {
-      url += '?api_key=8NNLSvVq9ozqkA1BA7KCey9ocE0iovWXs5dmjTu5';
+    if(url.search(/^https?:\/\/csp-api\.sam\.gov/) > -1) {
+      url += '?api_key=rkkGBk7AU8UQs9LHT6rM0rFkg3A3rGaiBntKSGEC';
     }
 
     return url;
