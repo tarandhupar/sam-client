79423d5b5abfce21298342a512fca1aa384fcdac
Anurag Mundada <anurag.mundada@C02RV0QXG8WL.local>
2017-04-24 14:58:34 -0400

    SAM-10170 Bug fix for populating dropdowns on refresh

--- src/app/search/search.spec.ts
+++ src/app/search/search.spec.ts
@@ -18,7 +18,7 @@ import { FHService } from '../../api-kit/fh/fh.service';
 import { PipesModule } from '../app-pipes/app-pipes.module';
 import { AlertFooterService } from '../alerts/alert-footer';
 import {SamContractTypeFilter} from "../awards/search-result/contract-type-filter/contract-type-filter.component";
-import {SamNaicsPscFilter} from "naics-psc-filter/naics-psc-filter.component";
+import {SamNaicsPscFilter} from "./naics-psc-filter/naics-psc-filter.component";
 
 let fixture;
 
@@ -93,7 +93,7 @@ let awardType = {
   }
 };
 
-describe('SearchPage', () => {
+fdescribe('SearchPage', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ SearchPage,OpportunitiesResult,AssistanceListingResult,FederalHierarchyResult,EntitiesResult,ExclusionsResult,WageDeterminationResult,AwardsResult,FHFeaturedResult, SamContractTypeFilter, SamNaicsPscFilter ],
@@ -184,6 +184,6 @@ describe('SearchPage', () => {
       expect(fixture.componentInstance.data.page.size).toBe(10);
       expect(fixture.componentInstance.data.page.totalElements).toBe(123);
     });
-  })
+  });
 
 });
