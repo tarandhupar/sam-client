ebd42fa78d0827ed094b72649b58beaf318472ad
Justin Babbs <justin.babbs@octoconsulting.com>
2017-05-02 13:46:45 -0400

    site wide cookie to fake out admin role

--- src/app/users/public/public.component.ts
+++ src/app/users/public/public.component.ts
@@ -13,7 +13,7 @@ export class UserViewComponent implements OnInit {
 
   ngOnInit() {
     this.userName = this.route.snapshot.params['id'];
-    if (/grant-access.*role-workspace/.test(this.router.url)) {
+    if (/grant-access/.test(this.router.url)) {
       this.hideNav = true;
     }
   }
