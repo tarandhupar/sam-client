f7df0feaa6db9294cb6d1db4fc762637e21e251a
diegoaruiz <diego@reisystems.com>
2017-05-02 18:01:41 -0400

    domain order update

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -4,12 +4,12 @@ export let globals = {
   showOptional : SHOW_OPTIONAL === 'true' || ENV === 'development',
   searchFilterConfig: [
     {value: '', label: 'All Domains', width: '130'},
-    {value: 'opp', label: 'Opportunities', width: '145'},
     {value: 'cfda', label: 'Assistance Listings', width: '180'},
-    {value: 'fh', label: 'Federal Hierarchy', width: '175'},
+    {value: 'opp', label: 'Opportunities', width: '145'},
+    {value: 'fpds', label: 'Awards', width: '100'},
     {value: 'ent', label: 'Entities', width: '100'},
     {value: 'ex', label: 'Exclusions', width: '120'},
+    {value: 'fh', label: 'Federal Hierarchy', width: '175'},
     {value: 'wd', label: 'Wage Determinations', width: '200'},
-    {value: 'fpds', label: 'Awards', width: '100'}
   ],
 };
