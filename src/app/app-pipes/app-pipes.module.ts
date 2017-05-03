d8638c5f86bdddb115c26c9ded6a7fff9f57ff92
Kevin Chiu <chiu_kevin@bah.com>
2017-05-03 17:09:47 -0400

    - Updated migrations history table with column heading updates and updated styling

--- src/app/app-pipes/app-pipes.module.ts
+++ src/app/app-pipes/app-pipes.module.ts
@@ -1,26 +1,30 @@
-import { KeysPipe } from './keyspipe.pipe';
+import { NgModule } from '@angular/core';
+
+import { CapitalizePipe } from './capitalize.pipe';
 import { DateFormatPipe } from './date-format.pipe';
 import { FilterMultiArrayObjectPipe } from './filter-multi-array-object.pipe';
-import { NgModule } from '@angular/core';
-import { CapitalizePipe } from './capitalize.pipe';
+import { GroupByPipe } from './group-by.pipe';
+import { KeysPipe } from './keyspipe.pipe';
 import { OrganizationTypeCodePipe } from './organization-type-code.pipe';
-import {SortArrayOfObjects} from "./sort-array-object.pipe";
+import { SortArrayOfObjects } from "./sort-array-object.pipe";
 
 @NgModule({
   imports: [],
   exports: [
-    FilterMultiArrayObjectPipe,
-    DateFormatPipe,
-    KeysPipe,
     CapitalizePipe,
+    DateFormatPipe,
+    FilterMultiArrayObjectPipe,
+    GroupByPipe,
+    KeysPipe,
     OrganizationTypeCodePipe,
     SortArrayOfObjects
   ],
   declarations: [
-    KeysPipe,
-    FilterMultiArrayObjectPipe,
-    DateFormatPipe,
     CapitalizePipe,
+    DateFormatPipe,
+    FilterMultiArrayObjectPipe,
+    GroupByPipe,
+    KeysPipe,
     OrganizationTypeCodePipe,
     SortArrayOfObjects
   ],
